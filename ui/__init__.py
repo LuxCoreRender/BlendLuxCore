ac3a3864b93f9b751e6074ba950b7389d7aa64ee
Simon Wendsche <Theverat@users.noreply.github.com>
2016-09-10 13:32:50 +0200

    ui sceleton

---
+++ ui/__init__.py
@@ -0,0 +1,22 @@
+import bpy
+
+def compatible_panels():
+    panels = [
+        "RENDER_PT_render",
+        "RENDER_PT_output",
+        "RENDER_PT_encoding",
+        "RENDER_PT_dimensions",
+        "RENDER_PT_stamp",
+    ]
+    types = bpy.types
+    return [getattr(types, p) for p in panels if hasattr(types, p)]
+
+
+def register():
+    for panel in compatible_panels():
+        panel.COMPAT_ENGINES.add('LUXCORE')
+
+
+def unregister():
+    for panel in compatible_panels():
+        panel.COMPAT_ENGINES.remove('LUXCORE')
