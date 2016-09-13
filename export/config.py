0891a44aee8a16da48e3c192815a0f020d883c8e
Simon Wendsche <Theverat@users.noreply.github.com>
2016-09-13 12:06:46 +0200

    first working session implementation (currently like Cycles does it)

--- export/config.py
+++ export/config.py
@@ -2,12 +2,13 @@ from ..bin import pyluxcore
 from .. import utils
 
 
-def convert(scene):
+def convert(scene, context=None):
     print("converting config")
     props = pyluxcore.Properties()
-    width, height = utils.calc_filmsize(scene)
+    width, height = utils.calc_filmsize(scene, context)
 
     props.Set(pyluxcore.Property("renderengine.type", "PATHCPU"))
+    props.Set(pyluxcore.Property("sampler.type", "SOBOL"))
     props.Set(pyluxcore.Property("film.width", width))
     props.Set(pyluxcore.Property("film.height", height))
 
