983060c468e19fd89a4a5f7371e30abf06540770
Simon Wendsche <simon.wendsche@gmx.de>
2017-12-14 18:05:03 +0100

    add a very basic point light test

---
+++ tests/lights/point.test.py
@@ -0,0 +1,20 @@
+import unittest
+
+# import the already loaded addon
+import BlendLuxCore
+from BlendLuxCore.export import light
+import bpy
+
+class TestPointLight(unittest.TestCase):
+    def test_prop_export(self):
+        # Test the property export
+        obj = bpy.data.objects["Point"]
+        props = light.convert(obj, bpy.context.scene)
+        # TODO: test for correctness (but implement object/light
+        # TODO: deletion first as it will probably change the signature)
+
+
+
+# we have to manually invoke the test runner here, as we cannot use the CLI
+suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestPointLight)
+unittest.TextTestRunner().run(suite)
--- tests/testrunner.py
+++ tests/testrunner.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python3
+
 import glob
 import subprocess
 import sys
+import os
 
-blenderExecutable = "blender"
+blender_executable = "blender"
 
 # allow override of blender executable (important for CI!)
 if len(sys.argv) > 1:
-    blenderExecutable = sys.argv[1]
+    blender_executable = sys.argv[1]
+
+if not os.path.exists(blender_executable):
+    print("Could not find Blender executable at path:", blender_executable)
+    exit(1)
 
 # iterate over each *.test.blend file in the "tests" directory
 # and open up blender with the .test.blend file and the corresponding .test.py python script
 for file in glob.glob("./**/*.test.blend"):
-  subprocess.call([blenderExecutable, "--addons", "BlendLuxCore",
+  subprocess.call([blender_executable, "--addons", "BlendLuxCore",
                    "--factory-startup", "-noaudio", "-b", file, "--python",
                    file.replace(".blend", ".py")])
