7ffef092725af041017a2653302262ffba8dc93e
Simon Wendsche <simon.wendsche@gmx.de>
2017-12-29 01:18:14 +0100

    add support for imagemap channel property

--- nodes/textures/imagemap.py
+++ nodes/textures/imagemap.py
@@ -1,5 +1,5 @@
 import bpy
-from bpy.props import PointerProperty
+from bpy.props import PointerProperty, EnumProperty
 from .. import LuxCoreNodeTexture
 from ...export.image import ImageExporter
 from ...utils import node as utils_node
@@ -7,9 +7,21 @@ from ...utils import node as utils_node
 
 class LuxCoreNodeTexImagemap(LuxCoreNodeTexture):
     bl_label = "Imagemap"
-    bl_width_min = 160
+    bl_width_min = 200
 
     image = PointerProperty(name="Image", type=bpy.types.Image)
+
+    channel_items = [
+        ("default", "Default", "Do not convert the image cannels"),
+        ("rgb", "RGB", "Use RGB color channels"),
+        ("red", "Red", "Use only the red color channel"),
+        ("green", "Green", "Use only the green color channel"),
+        ("blue", "Blue", "Use only the blue color channel"),
+        ("alpha", "Alpha", "Use only the alpha channel"),
+        ("mean", "Mean", "Greyscale"),
+        ("colored_mean", "Colored Mean", "Greyscale"),
+    ]
+    channel = EnumProperty(name="Channel", items=channel_items, default="default")
 
     def init(self, context):
         self.add_input("LuxCoreSocketFloatPositive", "Gamma", 2.2)
@@ -20,6 +32,9 @@ class LuxCoreNodeTexImagemap(LuxCoreNodeTexture):
 
     def draw_buttons(self, context, layout):
         layout.template_ID(self, "image", open="image.open")
+
+        if self.image:
+            layout.prop(self, "channel")
 
         # Info about UV mapping (only show if default is used,
         # when no mapping node is linked)
@@ -34,6 +49,7 @@ class LuxCoreNodeTexImagemap(LuxCoreNodeTexture):
             "file": ImageExporter.export(self.image),
             "gamma": self.inputs["Gamma"].export(props),
             "gain": self.inputs["Gain"].export(props),
+            "channel": self.channel,
             # Mapping
             "mapping.type": "uvmapping2d",
             "mapping.uvscale": uvscale,
