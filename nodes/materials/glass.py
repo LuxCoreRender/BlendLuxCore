74395672478a3d15c765c18c37f72f110b901d66
Simon Wendsche <simon.wendsche@gmx.de>
2018-01-04 14:34:38 +0100

    use the new cauchyc parameter for dispersion

--- nodes/materials/glass.py
+++ nodes/materials/glass.py
@@ -1,8 +1,17 @@
 from bpy.props import FloatProperty, BoolProperty
 from .. import LuxCoreNodeMaterial, Roughness
+from ..sockets import LuxCoreSocketFloat
 from ..output import get_active_output
-from .. import utils
-from ...utils import node as utils_node
+
+CAUCHYC_DESCRIPTION = (
+    "Dispersion strength (cauchy C coefficient)\n"
+    "Realistic values range from 0.00354 to 0.01342"
+)
+
+
+class LuxCoreSocketCauchyC(LuxCoreSocketFloat):
+    default_value = FloatProperty(name="Dispersion", default=0, min=0, soft_max=0.1, step=0.1, precision=5,
+                                  description=CAUCHYC_DESCRIPTION)
 
 
 class LuxCoreNodeMatGlass(LuxCoreNodeMaterial):
@@ -20,12 +29,12 @@ class LuxCoreNodeMatGlass(LuxCoreNodeMaterial):
     architectural = BoolProperty(name="Architectural",
                                  description="Skips refraction during transmission, propagates alpha and shadow rays",
                                  default=False)
-    dispersion = FloatProperty(name="Dispersion",description="Dispersion of medium, i.e. cauchy C coefficient)", default=0, min=0, soft_max=0.1, step=0.1, precision=5)
 
     def init(self, context):
         self.add_input("LuxCoreSocketColor", "Transmission Color", (1, 1, 1))
         self.add_input("LuxCoreSocketColor", "Reflection Color", (1, 1, 1))
         self.add_input("LuxCoreSocketIOR", "IOR", 1.5)
+        self.add_input("LuxCoreSocketCauchyC", "Dispersion", 0)
 
         Roughness.init(self, default=0.05, init_enabled=False)
         self.add_common_inputs()
@@ -44,7 +53,6 @@ class LuxCoreNodeMatGlass(LuxCoreNodeMaterial):
         row = layout.row()
         row.enabled = not self.rough
         row.prop(self, "architectural")
-        layout.prop(self, "dispersion")
 
         if self.get_interior_volume():
             layout.label("Using IOR of interior volume", icon="INFO")
@@ -61,58 +69,15 @@ class LuxCoreNodeMatGlass(LuxCoreNodeMaterial):
             "type": type,
             "kt": self.inputs["Transmission Color"].export(props),
             "kr": self.inputs["Reflection Color"].export(props),
-            "dispersion": self.dispersion > 0,
         }
 
-        # If we have an interior volume, we use its IOR instead of the glass IOR
-        interior_vol = self.get_interior_volume()
+        # Only use the glass node IOR socket if there is no interior volume
+        if not self.get_interior_volume():
+            definitions["interiorior"] = self.inputs["IOR"].export(props)
 
-        if interior_vol:
-            out = get_active_output(interior_vol, "LuxCoreNodeVolOutput")
-            vol_node = utils_node.get_linked_node(out.inputs["Volume"])
-            if vol_node:
-                ior = vol_node.inputs["IOR"].export(props)
-                is_textured_ior = vol_node.inputs["IOR"].is_linked
-            else:
-                raise Exception("ERROR in glass node export: can't find interior volume IOR")
-        else:
-            ior = self.inputs["IOR"].export(props)
-            is_textured_ior = self.inputs["IOR"].is_linked
-
-        if self.dispersion > 0:
-            # TODO: maybe we should scale the spread with IOR? (higher IOR, higher spread - and at IOR 1 a spread of 0)
-            if is_textured_ior:
-                # Create helper texture with unique name
-                node_tree = self.id_data
-                name_parts = ["dispersion", node_tree.name, self.name]
-                helper_name = utils.to_luxcore_name("_".join(name_parts))
-
-                helper_prefix = "scene.textures." + helper_name + "."
-
-                # Info for calculations:
-                # cauchy_b = ior-self.dispersion/0.589**2
-                # IOR_Red = cauchy_b+self.dispersion/0.645**2
-                # IOR_Green = cauchy_b+self.dispersion/0.510**2
-                # IOR_Blue = cauchy_b+self.dispersion/0.440**2
-                helper_defs = {
-                    "type": "add",
-                    "texture1": ior,
-                    "texture2": [-self.dispersion/0.589**2+self.dispersion/0.645**2, -self.dispersion/0.589**2+self.dispersion/0.510**2, -self.dispersion/0.589**2+self.dispersion/0.440**2]
-                }
-                props.Set(utils.create_props(helper_prefix, helper_defs))
-
-                definitions["interiorior"] = helper_name
-            else:
-                cauchy_b = ior - self.dispersion/0.589**2
-
-                # IOR is not textured, just a simple value
-                # Prevent IOR below 1 (would lead to weird results)
-                red = cauchy_b + self.dispersion/0.645**2
-                green = cauchy_b + self.dispersion/0.510**2
-                blue = cauchy_b + self.dispersion/0.440**2
-                definitions["interiorior"] = [red, green, blue]
-        else:
-            definitions["interiorior"] = ior
+        cauchyc = self.inputs["Dispersion"].export(props)
+        if self.inputs["Dispersion"].is_linked or cauchyc > 0:
+            definitions["cauchyc"] = cauchyc
 
         if self.rough:
             Roughness.export(self, props, definitions)
