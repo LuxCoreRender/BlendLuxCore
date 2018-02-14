f6312155662a108ad7e5b349d27fd8a0294a67d1
Simon Wendsche <simon.wendsche@gmx.de>
2018-02-15 00:30:22 +0100

    add epsilon to non-invertible matrices (prevent runtime error)

--- export/duplis.py
+++ export/duplis.py
@@ -17,79 +17,95 @@ class Duplis:
 
 
 def convert(blender_obj, scene, context, luxcore_scene, engine=None):
-    assert blender_obj.is_duplicator
+    try:
+        assert blender_obj.is_duplicator
 
-    dupli_props = pyluxcore.Properties()
+        dupli_props = pyluxcore.Properties()
 
-    if not utils.is_obj_visible(blender_obj, scene, context):
-        # Emitter is not on a visible layer
-        return
+        if not utils.is_obj_visible(blender_obj, scene, context):
+            # Emitter is not on a visible layer
+            return
 
-    start = time()
+        start = time()
 
-    mode = 'VIEWPORT' if context else 'RENDER'
-    blender_obj.dupli_list_create(scene, settings=mode)
+        mode = 'VIEWPORT' if context else 'RENDER'
+        blender_obj.dupli_list_create(scene, settings=mode)
 
-    name_prefix = utils.get_luxcore_name(blender_obj, context)
-    exported_duplis = {}
+        name_prefix = utils.get_luxcore_name(blender_obj, context)
+        exported_duplis = {}
+        non_invertible_count = 0
 
-    dupli_count = len(blender_obj.dupli_list)
-    for i, dupli in enumerate(blender_obj.dupli_list):
-        # Metaballs are omitted from this loop, they cause glitches.
-        if dupli.object.type == "META":
-            continue
-        # Objects with non-invertible matrices cannot be loaded by LuxCore (RuntimeError)
-        if dupli.matrix.determinant() == 0:
-            continue
+        dupli_count = len(blender_obj.dupli_list)
+        for i, dupli in enumerate(blender_obj.dupli_list):
+            # Metaballs are omitted from this loop, they cause glitches.
+            if dupli.object.type == "META":
+                continue
 
-        # Use the utils functions to build names so linked objects work (libraries)
-        name = name_prefix + utils.get_luxcore_name(dupli.object, context)
-        matrix_list = utils.matrix_to_list(dupli.matrix, scene, apply_worldscale=True)
+            if dupli.matrix.determinant() == 0:
+                # We can handle non-invertible matrices (a small epsilon is added)
+                # but warn the user later because it's a sign of trouble
+                non_invertible_count += 1
 
-        try:
-            # Already exported, just update the Duplis info
-            exported_duplis[name].add(matrix_list)
-        except KeyError:
-            # Not yet exported
-            name_suffix = name_prefix + str(dupli.index)
-            if dupli.particle_system:
-                name_suffix += utils.get_luxcore_name(dupli.particle_system, context)
+            # Use the utils functions to build names so linked objects work (libraries)
+            name = name_prefix + utils.get_luxcore_name(dupli.object, context)
+            matrix_list = utils.matrix_to_list(dupli.matrix, scene, apply_worldscale=True)
 
-            obj_props, exported_obj = blender_object.convert(dupli.object, scene, context, luxcore_scene,
-                                                             update_mesh=True, dupli_suffix=name_suffix)
-            dupli_props.Set(obj_props)
-            exported_duplis[name] = Duplis(exported_obj, matrix_list)
+            try:
+                # Already exported, just update the Duplis info
+                exported_duplis[name].add(matrix_list)
+            except KeyError:
+                # Not yet exported
+                name_suffix = name_prefix + str(dupli.index)
+                if dupli.particle_system:
+                    name_suffix += utils.get_luxcore_name(dupli.particle_system, context)
 
-        # Report progress and check if user wants to cancel export
-        # Note: in viewport render we can't do all this, so we don't pass the engine there
-        if engine and i % 1000 == 0:
-            progress = (i / dupli_count) * 100
-            engine.update_stats("Export", "Object: %s (Duplis: %d%%)" % (blender_obj.name, progress))
+                obj_props, exported_obj = blender_object.convert(dupli.object, scene, context, luxcore_scene,
+                                                                 update_mesh=True, dupli_suffix=name_suffix)
+                dupli_props.Set(obj_props)
+                exported_duplis[name] = Duplis(exported_obj, matrix_list)
 
-            if engine.test_break():
-                blender_obj.dupli_list_clear()
-                return
+            # Report progress and check if user wants to cancel export
+            # Note: in viewport render we can't do all this, so we don't pass the engine there
+            if engine and i % 1000 == 0:
+                progress = (i / dupli_count) * 100
+                engine.update_stats("Export", "Object: %s (Duplis: %d%%)" % (blender_obj.name, progress))
 
-    blender_obj.dupli_list_clear()
-    # Need to parse so we have the dupli objects available for DuplicateObject
-    luxcore_scene.Parse(dupli_props)
+                if engine.test_break():
+                    blender_obj.dupli_list_clear()
+                    return
 
-    for duplis in exported_duplis.values():
-        # exported_obj sometimes is None, e.g. when instancing a group using an empty
-        if duplis.exported_obj:
-            # Objects might be split if they have multiple materials
-            for src_name in duplis.exported_obj.luxcore_names:
-                dst_name = src_name + "dupli"
-                count = duplis.count
-                transformations = array("f", duplis.matrices)
-                luxcore_scene.DuplicateObject(src_name, dst_name, count, transformations)
+        if non_invertible_count:
+            msg = (
+                '%d duplis with non-invertible matrices on duplicator "%s". '
+                'This can happen if e.g. the scale is 0' % (non_invertible_count, blender_obj.name)
+            )
+            scene.luxcore.errorlog.add_warning(msg)
 
-                # TODO: support steps and times (motion blur)
-                # steps = 0 # TODO
-                # times = array("f", [])
-                # luxcore_scene.DuplicateObject(src_name, dst_name, count, steps, times, transformations)
+        blender_obj.dupli_list_clear()
+        # Need to parse so we have the dupli objects available for DuplicateObject
+        luxcore_scene.Parse(dupli_props)
 
-                # Delete the object we used for duplication, we don't want it to show up in the scene
-                luxcore_scene.DeleteObject(src_name)
+        for duplis in exported_duplis.values():
+            # exported_obj sometimes is None, e.g. when instancing a group using an empty
+            if duplis.exported_obj:
+                # Objects might be split if they have multiple materials
+                for src_name in duplis.exported_obj.luxcore_names:
+                    dst_name = src_name + "dupli"
+                    count = duplis.count
+                    transformations = array("f", duplis.matrices)
+                    luxcore_scene.DuplicateObject(src_name, dst_name, count, transformations)
 
-    print("Dupli export took %.3fs" % (time() - start))
+                    # TODO: support steps and times (motion blur)
+                    # steps = 0 # TODO
+                    # times = array("f", [])
+                    # luxcore_scene.DuplicateObject(src_name, dst_name, count, steps, times, transformations)
+
+                    # Delete the object we used for duplication, we don't want it to show up in the scene
+                    luxcore_scene.DeleteObject(src_name)
+
+        print("Dupli export took %.3fs" % (time() - start))
+    except Exception as error:
+        msg = "[%s] %s" % (blender_obj.name, error)
+        scene.luxcore.errorlog.add_warning(msg)
+        import traceback
+        traceback.print_exc()
--- utils/__init__.py
+++ utils/__init__.py
@@ -139,14 +139,23 @@ def matrix_to_list(matrix, scene=None, apply_worldscale=False, invert=False):
         matrix = get_scaled_to_world(matrix, scene)
 
     if invert:
-        matrix = matrix.inverted()
+        matrix.invert_safe()
 
     l = [matrix[0][0], matrix[1][0], matrix[2][0], matrix[3][0],
          matrix[0][1], matrix[1][1], matrix[2][1], matrix[3][1],
          matrix[0][2], matrix[1][2], matrix[2][2], matrix[3][2],
          matrix[0][3], matrix[1][3], matrix[2][3], matrix[3][3]]
 
-    return [float(i) for i in l]
+    if matrix.determinant() == 0:
+        # The matrix is non-invertible. This can happen if e.g. the scale on one axis is 0.
+        # Prevent a RuntimeError from LuxCore by adding a small epsilon.
+        msg = "Non-invertible matrix. Can happen if e.g. an object has scale 0"
+        bpy.context.scene.luxcore.errorlog.add_warning(msg)
+
+        epsilon = 1e-5
+        return [float(i) + epsilon for i in l]
+    else:
+        return [float(i) for i in l]
 
 
 def calc_filmsize_raw(scene, context=None):
