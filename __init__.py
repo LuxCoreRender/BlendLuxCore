7c392b6d9eea6d7df6fb6ea7eafec019d8b47d68
Simon Wendsche <simon.wendsche@gmx.de>
2018-02-15 09:42:59 +0100

    only sync material/node tree names every second to reduce CPU load when idle

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from time import time
 import bpy
 from bpy.app.handlers import persistent
 from .bin import pyluxcore
@@ -44,8 +45,20 @@ def luxcore_load_post(_):
     compatibility.run()
 
 
+# We only sync material and node tree names every second to reduce CPU load
+NAME_UPDATE_INTERVAL = 1  # seconds
+last_name_update = time()
+
 @persistent
 def luxcore_scene_update_post(scene):
+    global last_name_update
+    if time() - last_name_update < NAME_UPDATE_INTERVAL:
+        return
+    last_name_update = time()
+
+    # If material name was changed, rename the node tree, too.
+    # Note that bpy.data.materials.is_updated is always False here
+    # so we can't use it as fast check.
     for mat in bpy.data.materials:
         node_tree = mat.luxcore.node_tree
 
