28bf75c7a238566e652a92466341e189bc6429ef
Simon Wendsche <simon.wendsche@gmx.de>
2018-02-16 14:43:19 +0100

    add Philstix to authors and bump version number to alpha5

--- __init__.py
+++ __init__.py
@@ -13,13 +13,13 @@ from .ui import (aovs, blender_object, camera, config, display, errorlog,
 
 bl_info = {
     "name": "LuxCore",
-    "author": "Simon Wendsche (B.Y.O.B.), Michael Klemm (neo2068)",
+    "author": "Simon Wendsche (B.Y.O.B.), Michael Klemm (neo2068), Philstix",
     "version": (2, 0),
     "blender": (2, 79, 0),
     "category": "Render",
     "location": "Info header, render engine menu",
     "description": "LuxCore integration for Blender",
-    "warning": "alpha4",
+    "warning": "alpha5",
     "wiki_url": "https://wiki.luxcorerender.org/",
     "tracker_url": "https://github.com/LuxCoreRender/BlendLuxCore/issues/new",
 }
