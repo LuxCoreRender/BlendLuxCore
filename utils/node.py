f9a4f100ef0baa6e75881706d0ea1dde5a6db096
Simon Wendsche <simon.wendsche@gmx.de>
2018-05-05 11:46:22 +0200

    fix incorrect warning about missing uv maps on all objects except MESH

--- utils/node.py
+++ utils/node.py
@@ -8,15 +8,25 @@ def draw_uv_info(context, layout):
     Call this function on nodes that use UV mapping (e.g. the Roughness class uses it
     when anisotropic roughness is enabled because it requires UV mapping).
     """
-    if context.object.data:
-        uv_textures = getattr(context.object.data, "uv_textures", [])
-        if len(uv_textures) > 1:
-            box = layout.box()
-            box.label("LuxCore only supports one UV map", icon="INFO")
-            active_uv = find_active_uv(context.object.data.uv_textures)
-            box.label('Active: "%s"' % active_uv.name, icon="GROUP_UVS")
-        elif len(uv_textures) == 0:
-            layout.label("No UV map", icon="ERROR")
+    obj = context.object
+
+    def warning_no_uvmap():
+        layout.label("No UV map", icon="ERROR")
+
+    if obj and obj.data:
+        if obj.type in {"CURVE", "SURFACE", "FONT"}:
+            if not obj.data.use_uv_as_generated:
+                warning_no_uvmap()
+        elif obj.type == "MESH":
+            if len(obj.data.uv_textures) > 1:
+                box = layout.box()
+                box.label("LuxCore only supports one UV map", icon="INFO")
+                active_uv = find_active_uv(obj.data.uv_textures)
+                box.label('Active: "%s"' % active_uv.name, icon="GROUP_UVS")
+            elif len(obj.data.uv_textures) == 0:
+                warning_no_uvmap()
+        else:
+            warning_no_uvmap()
 
 
 def draw_transmission_info(node, layout):
