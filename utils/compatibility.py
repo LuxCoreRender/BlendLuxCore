b62f9a45da1905bf03fc9c85a1ba45b0af49a53b
Simon Wendsche <simon.wendsche@gmx.de>
2018-05-12 15:24:39 +0200

    remove the old min/max sockets on colormix node instead of disabling them

--- utils/compatibility.py
+++ utils/compatibility.py
@@ -124,8 +124,10 @@ def update_colormix_remove_min_max_sockets():
 
         for node in find_nodes(node_tree, "LuxCoreNodeTexColorMix"):
             if node.mode == "clamp" and "Min" in node.inputs and "Max" in node.inputs:
-                node.mode_clamp_min = node.inputs["Min"].default_value
-                node.mode_clamp_max = node.inputs["Max"].default_value
-                node.inputs["Min"].enabled = False
-                node.inputs["Max"].enabled = False
+                socket_min = node.inputs["Min"]
+                socket_max = node.inputs["Max"]
+                node.mode_clamp_min = socket_min.default_value
+                node.mode_clamp_max = socket_max.default_value
+                node.inputs.remove(socket_min)
+                node.inputs.remove(socket_max)
                 print('Updated %s node "%s" in tree "%s" to new version' % (node.bl_idname, node.name, node_tree.name))
