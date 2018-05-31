4d7538573a6d5771c66b9ce4b9c11a7505e0eb84
Simon Wendsche <simon.wendsche@gmx.de>
2018-05-31 21:34:22 +0200

    some improvents to the denoiser help text

--- handlers/draw_imageeditor.py
+++ handlers/draw_imageeditor.py
@@ -10,6 +10,13 @@ def handler():
     if context.scene.render.engine != "LUXCORE":
         return
 
+    _denoiser_help_text(context)
+
+
+def _denoiser_help_text(context):
+    if not context.scene.luxcore.denoiser.enabled:
+        return
+
     # Only show the help text if the toolshelf is not visible
     for area in context.screen.areas:
         if area.type == 'IMAGE_EDITOR':
@@ -37,15 +44,17 @@ def handler():
     if image_user.multilayer_pass == denoised_pass_index:
         # From https://github.com/sftd/blender-addons-contrib/blob/master/render_time.py
         font_id = 0  # XXX, need to find out how best to get this. [sic]
+        ui_scale = context.user_preferences.view.ui_scale
+        dpi = round(72 * ui_scale)
 
-        pos_y = 45
+        pos_y = 45 * ui_scale
         blf.position(font_id, 15, pos_y, 0)
-        blf.size(font_id, 18, 72)
+        blf.size(font_id, 14, dpi)
         blf.enable(font_id, blf.SHADOW)
         blf.shadow(font_id, 5, 0.0, 0.0, 0.0, 1.0)
 
         blf.draw(font_id, '← The denoiser controls are in the "LuxCore" category')
-        pos_y -= 24
+        pos_y -= 20 * ui_scale
         blf.position(font_id, 15, pos_y, 0)
         blf.draw(font_id, 'in the tool shelf, (press T)')
 
