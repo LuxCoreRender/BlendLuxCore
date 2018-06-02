dd5a4ff2d76277203e44c1a5c04f1a08165b1f81
Simon Wendsche <simon.wendsche@gmx.de>
2018-06-02 13:47:57 +0200

    allow chained attribute calls, show error messages

--- nodes/textures/propertyaccess.py
+++ nodes/textures/propertyaccess.py
@@ -1,42 +1,68 @@
 import bpy
 from bpy.props import StringProperty, PointerProperty
 from .. import LuxCoreNodeTexture
+from ...utils import ui as utils_ui
 
 
 class LuxCoreNodeTexPropertyAccess(LuxCoreNodeTexture):
     bl_label = "PropertyAccess"
 
     datablock = PointerProperty(name="Datablock", type=bpy.types.Object)
-    prop = StringProperty(name="Prop")
+    attribute_path = StringProperty(name="Attribute")
+    error = StringProperty(name="Error")
 
     def init(self, context):
         self.outputs.new("LuxCoreSocketFloatUnbounded", "Value")
 
     def draw_label(self):
-        if self.datablock and self.prop:
-            return self.datablock.name + "." + self.prop
+        if self.datablock and self.attribute_path:
+            return self.datablock.name + "." + self.attribute_path
         else:
             return self.bl_label
 
     def draw_buttons(self, context, layout):
         layout.prop(self, "datablock")
-        layout.prop(self, "prop")
+        layout.prop(self, "attribute_path")
+        if self.error:
+            row = layout.row()
+            row.label("", icon="CANCEL")
+            row.prop(self, "error")
 
     def sub_export(self, exporter, props, luxcore_name=None):
-        if self.datablock and self.prop:
-            value = getattr(self.datablock, self.prop, 0)
+        self.error = ""
+        utils_ui.tag_region_for_redraw(bpy.context, "NODE_EDITOR", "WINDOW")
 
-            try:
-                value = list(value)
-            except TypeError:
-                # Not iterable
-                value = [value, value, value]
-        else:
-            value = [0, 0, 0]
+        try:
+            if self.datablock and self.attribute_path:
+                value = self.datablock
+                for attrib in self.attribute_path.split("."):
+                    value = getattr(value, attrib)
 
-        definitions = {
-            "type": "constfloat3",
-            "value": value,
-        }
+                try:
+                    value = list(value)
+                except TypeError:
+                    # Not iterable
+                    value = [value, value, value]
+            else:
+                value = [0, 0, 0]
 
-        return self.create_props(props, definitions, luxcore_name)
+            definitions = {
+                "type": "constfloat3",
+                "value": value,
+            }
+
+            return self.create_props(props, definitions, luxcore_name)
+        except Exception as error:
+            import traceback
+            traceback.print_exc()
+
+            self.error = str(error)
+            utils_ui.tag_region_for_redraw(bpy.context, "NODE_EDITOR", "WINDOW")
+
+            definitions = {
+                "type": "constfloat1",
+                "value": 0,
+            }
+
+            return self.create_props(props, definitions, luxcore_name)
+
