6d479373fcf49834dcca606c2739c520a000e23e
Simon Wendsche <simon.wendsche@gmx.de>
2018-06-12 20:07:08 +0200

    clear modifiers on proxy, remove all orphaned data after proxy creation

--- operators/blender_object.py
+++ operators/blender_object.py
@@ -7,6 +7,28 @@ from .utils import (
     LUXCORE_OT_set_node_tree, 
 )
 
+# TODO:
+# - Undo handling
+# - Split into smaller functions
+# - Cleanup
+# - Decimate percentage option
+# - Support all surface types, not only MESH
+# - Test with all kinds of objects, curves, text, empty etc.
+
+
+def remove(data):
+    if type(data) == bpy.types.Mesh:
+        bpy.data.meshes.remove(data)
+    elif type(data) in {bpy.types.Curve, bpy.types.TextCurve, bpy.types.SurfaceCurve}:
+        bpy.data.curves.remove(data)
+    elif type(data) == bpy.types.MetaBall:
+        bpy.data.metaballs.remove(data)
+    elif data is None:
+        pass
+    else:
+        print("Could not remove datablock %s (type %s)" % (data.name, type(data)))
+
+
 def LUXCORE_OT_use_proxy_switch(self, context):
     obj = context.active_object
     transformation = obj.matrix_world
@@ -62,14 +84,12 @@ class LUXCORE_OT_proxy_new(bpy.types.Operator):
         if obj.type in ['MESH']:
             #Copy object
             print("Create Proxy: Copy object")
-            proxy = obj
-            obj = proxy.copy()
-            obj.data = proxy.data.copy()
-            context.scene.objects.link(obj)
+            # TODO we need to make sure that we create a MESH object, even if source is e.g. a CURVE
+            proxy = obj.copy()
+            context.scene.objects.link(proxy)
 
             # rename object
-            obj.name = proxy.name
-            proxy.name = obj.name + '_lux_proxy'
+            proxy.name = obj.name + "_lux_proxy"
 
             # TODO: accept custom parameters for decimate modifier
             decimate = proxy.modifiers.new('proxy_decimate', 'DECIMATE')
@@ -77,19 +97,22 @@ class LUXCORE_OT_proxy_new(bpy.types.Operator):
 
             # Create low res proxy object
             print("Create Proxy: Create low res proxy object")
-            proxy.select = True
-            context.scene.objects.active = proxy
-            #bpy.ops.object.modifier_apply(apply_as='DATA', modifier=decimate.name)
+            proxy_mesh = proxy.to_mesh(context.scene, True, 'PREVIEW')
+            proxy.modifiers.clear()
+            proxy.data = proxy_mesh
             proxy.luxcore.use_proxy = True
 
+            # clear parent
             bpy.ops.object.select_all(action='DESELECT')
             obj.select = True
             context.scene.objects.active = obj
-            
-            # clear parent
             bpy.ops.object.parent_clear(type = 'CLEAR_KEEP_TRANSFORM')
 
             mesh = obj.to_mesh(context.scene, True, 'RENDER')
+            mesh_name = utils.to_luxcore_name(obj.name)
+            # Delete the original object, we don't need it anymore
+            remove(obj.data)
+            bpy.data.objects.remove(obj, do_unlink=True)
             
             # Export object into PLY files via pyluxcore functions
             luxcore_scene = pyluxcore.Scene()
@@ -110,15 +133,16 @@ class LUXCORE_OT_proxy_new(bpy.types.Operator):
             else:
                 vertexColors = 0            
             
-            mesh_definitions = luxcore_scene.DefineBlenderMesh(obj.name, len(mesh.tessfaces), faces, len(mesh.vertices),
-                                           vertices, texCoords, vertexColors, None)
+            mesh_definitions = luxcore_scene.DefineBlenderMesh(mesh_name, len(mesh.tessfaces), faces,
+                                                               len(mesh.vertices), vertices,
+                                                               texCoords, vertexColors, None)
+            # Delete the temporary mesh (don't have to unlink because it was never "registered" in bpy.data)
+            bpy.data.meshes.remove(mesh, do_unlink=False)
 
-            bpy.ops.object.delete()
-            
             print("Create Proxy: Export high resolution geometry data into PLY files...")
             for [name, mat] in mesh_definitions:
                 filepath = self.directory + name + ".ply"
-                luxcore_scene.SaveMesh("Mesh-"+name, filepath);                
+                luxcore_scene.SaveMesh("Mesh-" + name, filepath)
                 new = proxy.luxcore.proxies.add()
                 new.name = name
                 new.matIndex = mat
