19094f367d130f678f80be7479bfd083100e065a
Simon Wendsche <simon.wendsche@gmx.de>
2018-07-25 02:00:16 +0200

    working on more/improved wrap modes

--- properties/image_user.py
+++ properties/image_user.py
@@ -1,7 +1,15 @@
+import random
 import bpy
 from bpy.types import PropertyGroup
-from bpy.props import IntProperty, BoolProperty, PointerProperty
+from bpy.props import IntProperty, BoolProperty, PointerProperty, EnumProperty
 from .. import utils
+
+
+WRAP_NONE_DESC = (
+    "No wrapping is performed. If the image sequence does not contain enough "
+    "frames to cover the whole animation, some frames will be missing. "
+    "Only use this option if the sequence contains enough frames"
+)
 
 
 class LuxCoreImageUser(PropertyGroup):
@@ -16,14 +24,23 @@ class LuxCoreImageUser(PropertyGroup):
     # This image reference is just for internal bookkeeping
     image = PointerProperty(type=bpy.types.Image)
 
-    frame_duration = IntProperty(name="Frames", min=1, default=1,
-                                 description="Number of frames of a sequence to use")
-    frame_start = IntProperty(name="Start Frame", default=1,
-                              description="Global starting frame of the sequence, assuming first picture has a #1")
-    frame_offset = IntProperty(name="Offset", default=0,
-                               description="Offset the number of the frame to use in the animation")
-    use_cyclic = BoolProperty(name="Cyclic", default=False,
-                              description="Cycle the images in the sequence")
+    # TODO descriptions
+    first_frame = IntProperty(name="First Frame", default=1)
+    last_frame = IntProperty(name="Last Frame", default=2)
+    frame_offset = IntProperty(name="Offset", default=0)
+
+    pick_random = BoolProperty(name="Pick Random", default=False)
+    seed = IntProperty(name="Seed", default=0)
+    reverse = BoolProperty(name="Reverse", default=False)
+
+    wrap_modes = [
+        ("none", "None", WRAP_NONE_DESC, 0),
+        ("clamp", "Clamp", "Use the first (or last) frame of the sequence", 1),
+        ("repeat", "Repeat", "Loop the sequence", 2),
+        ("pingpong", "Ping-Pong", "Loop the sequence reversed", 3),
+    ]
+    wrap_mode = EnumProperty(name="Wrap", items=wrap_modes, default="clamp",
+                             description="How to handle the case of being outside of the sequence range")
 
     def update(self, image):
         """ This function should be called on each image update """
@@ -35,35 +52,57 @@ class LuxCoreImageUser(PropertyGroup):
 
                 if indexed_filepaths:
                     first_index, first_path = indexed_filepaths[0]
-                    self.frame_offset = first_index - 1
-                    self.frame_duration = len(indexed_filepaths)
+                    frame_count = len(indexed_filepaths)
+                    self.first_frame = 1
+                    self.last_frame = frame_count
+                    self.frame_offset = -first_index + 1
         self.image = image
 
     def get_frame(self, scene):
-        frame = scene.frame_current
-        frame = frame - self.frame_start + 1
+        frame = scene.frame_current + self.frame_offset
 
-        if self.use_cyclic:
-            frame %= self.frame_duration
-            if frame < 0:
-                frame += self.frame_duration
-            if frame == 0:
-                frame = self.frame_duration
+        if self.pick_random:
+            random.seed(self.seed ^ frame)
+            return random.randint(self.first_frame, self.last_frame)
 
-        if frame < 1:
-            frame = 1
-        elif frame > self.frame_duration:
-            frame = self.frame_duration
+        frame_count = self.last_frame - (self.first_frame - 1)
 
-        frame += self.frame_offset
+        if self.wrap_mode == "clamp":
+            frame = utils.clamp(frame, self.first_frame, self.last_frame)
+        elif self.wrap_mode == "repeat":
+            frame -= 1
+            frame %= frame_count
+            frame += 1
+        elif self.wrap_mode == "pingpong":
+            # TODO
+            frame -= 1
+            frame %= frame_count * 2
+            frame = frame_count - abs(frame - frame_count)
+            # frame += 1
+
+        if self.reverse:
+            frame = frame_count - frame + 1
+
         return frame
 
     def draw(self, layout, scene):
         if self.image and self.image.source == "SEQUENCE":
             box = layout.box()
             sub = box.column(align=True)
-            sub.label("Frame: %d" % self.get_frame(scene))
-            sub.prop(self, "frame_duration")
-            sub.prop(self, "frame_start")
+
+            frame = self.get_frame(scene)
+            sub.label("Frame: %d" % frame)
+            if frame < self.first_frame or frame > self.last_frame:
+                sub.label("Out of range", icon="ERROR")
+
+            sub.prop(self, "first_frame")
+            sub.prop(self, "last_frame")
             sub.prop(self, "frame_offset")
-            sub.prop(self, "use_cyclic")
+            sub.prop(self, "pick_random")
+            if self.pick_random:
+                sub.prop(self, "seed")
+
+            sub2 = sub.column(align=True)
+            sub2.active = not self.pick_random
+            sub2.prop(self, "reverse")
+            sub2.prop(self, "wrap_mode")
