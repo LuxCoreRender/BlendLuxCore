f9d9d71aeb8361154753ea95c9266e951a7d1427
Michael Klemm <neo2068@web.de>
2018-08-22 20:35:51 +0200

    First work for refactoring proxy handling via mesh data. Not functional yet!

--- properties/blender_object.py
+++ properties/blender_object.py
@@ -1,30 +1,17 @@
 import bpy
 from bpy.props import BoolProperty, IntProperty, PointerProperty, StringProperty, CollectionProperty
 from bpy.types import PropertyGroup
-from ..operators.blender_object import LUXCORE_OT_use_proxy_switch
 
 DESC_VISIBLE_TO_CAM = (
     "If disabled, the object will not be visible to camera rays. "
     "Note that it will still be visible in indirect light, shadows and reflections"
 )
 DESC_MOTION_BLUR = "Export this object as instance if object motion blur is enabled in camera settings"
-DESC_USE_PROXY = "Use the object as a proxy for quick viewport response"
-DESC_PROXIES = "Filepath to the high res objects used in rendering"
-DESC_FILEPATH = "Path to a .ply file which is loaded at render time and replaces the low-poly proxy object"
-
 
 def init():
     bpy.types.Object.luxcore = PointerProperty(type=LuxCoreObjectProps)
 
 
-class LuxCoreProxyList(PropertyGroup):
-    name = StringProperty()
-    matIndex = IntProperty()
-    filepath = StringProperty(subtype='FILE_PATH', description=DESC_FILEPATH)
-
-
 class LuxCoreObjectProps(PropertyGroup):
     visible_to_camera = BoolProperty(name="Visible to Camera", default=True, description=DESC_VISIBLE_TO_CAM)
     enable_motion_blur = BoolProperty(name="Motion Blur", default=True, description=DESC_MOTION_BLUR)
-    use_proxy = BoolProperty(name="Use as Proxy", default=False, update=LUXCORE_OT_use_proxy_switch, description=DESC_USE_PROXY)
-    proxies = CollectionProperty(name="Proxy Files", type=LuxCoreProxyList, description=DESC_PROXIES)    
