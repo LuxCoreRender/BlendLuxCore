9631de2d94b05cff1cf8653f79019d14f475a389
Simon Wendsche <simon.wendsche@gmx.de>
2018-09-24 18:06:40 +0200

    improve UI of blackbody node

--- nodes/textures/blackbody.py
+++ nodes/textures/blackbody.py
@@ -1,22 +1,21 @@
-import bpy
 from bpy.props import FloatProperty
 from .. import LuxCoreNodeTexture
-from ... import utils
+
 
 class LuxCoreNodeTexBlackbody(LuxCoreNodeTexture):
     bl_label = "Blackbody"
     bl_width_default = 200
 
-    temperature = FloatProperty(name="Temperature", description="Blackbody Temperature", default=6500.0, min=0.0, soft_max=10000.0)
+    temperature = FloatProperty(name="Temperature", description="Blackbody Temperature",
+                                default=6500, min=0, soft_max=10000, step=10)
     
     def init(self, context):
         self.outputs.new("LuxCoreSocketColor", "Color")
 
     def draw_buttons(self, context, layout):
-        layout.prop(self, "temperature")
+        layout.prop(self, "temperature", slider=True)
     
-    def sub_export(self, exporter, props, luxcore_name=None):        
-
+    def sub_export(self, exporter, props, luxcore_name=None):
         definitions = {
             "type": "blackbody",
             "temperature": self.temperature,
