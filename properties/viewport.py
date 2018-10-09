2d7491b1e010ffbae1b9e898877f8c779d80c222
Simon Wendsche <simon.wendsche@gmx.de>
2018-10-09 16:21:37 +0200

    add option to use Bidir in viewport (enabled by default)

--- properties/viewport.py
+++ properties/viewport.py
@@ -38,3 +38,9 @@ class LuxCoreViewportSettings(bpy.types.PropertyGroup):
     reduce_resolution_on_edit = BoolProperty(name="Reduce first sample resolution", default=True,
                                              description="Render the first sample after editing the scene "
                                                          "with reduced resolution to provide a quicker response")
+
+    use_bidir = BoolProperty(name="Use Bidir in Viewport", default=True,
+                             description="Enable if your scene requires Bidir for complex light paths and "
+                                         "you need to preview them in the viewport render. If disabled, "
+                                         "the RT Path engine is used in the viewport, which is optimized "
+                                         "for quick feedback but can't handle complex light paths")
