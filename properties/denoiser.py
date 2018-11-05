4d902b7a7e35b7f4a4dfa56a03c14f343e880ff2
Theverat <simonwendsche@gmx.de>
2018-11-05 16:22:48 +0100

    add some soft limits for denoiser settings

--- properties/denoiser.py
+++ properties/denoiser.py
@@ -3,11 +3,17 @@ from bpy.props import (
     BoolProperty, IntProperty, FloatProperty,
 )
 
+REFRESH_DESC = (
+    "Update the denoised image (takes a few seconds to minutes, "
+    "progress is shown in the status bar)"
+)
 SCALES_DESC = (
-    "Try to use more scales if the denoised image shows blotchiness (higher values increase computation time)"
+    "Try to use more scales if the denoised image shows blotchiness "
+    "(higher values increase computation time and RAM usage)"
 )
 HIST_DIST_THRESH_DESC = (
-    "A lower value will make the output sharper while a value higher than 1.0 will make the result more blurry"
+    "A lower value will make the output sharper while a value higher "
+    "than 1.0 will make the result more blurry"
 )
 SEARCH_WINDOW_RADIUS_DESC = (
     "Higher values improve the denoiser result, but lead to longer computation time"
@@ -16,23 +22,20 @@ FILTER_SPIKES_DESC = "Filter outliers from the input samples"
 
 
 class LuxCoreDenoiser(PropertyGroup):
-    # TODO: maybe an easy mode or some presets
-
     enabled = BoolProperty(name="", default=False, description="Enable/disable denoiser")
     show_advanced = BoolProperty(name="Show Advanced", default=False,
                                  description="Show advanced settings. They usually don't need to get tweaked")
 
     refresh = BoolProperty(name="Run Denoiser", default=False,
-                           description="Update the denoised image (takes a few seconds to minutes, "
-                                       "progress is shown in the status bar)")
+                           description=REFRESH_DESC)
 
-    scales = IntProperty(name="Scales", default=3, min=1,
+    scales = IntProperty(name="Scales", default=3, min=1, soft_max=5,
                          description=SCALES_DESC)
-    hist_dist_thresh = FloatProperty(name="Histogram Distance Threshold", default=1, min=0,
+    hist_dist_thresh = FloatProperty(name="Histogram Distance Threshold", default=1, min=0, soft_max=3,
                                      description=HIST_DIST_THRESH_DESC)
     # TODO: description for patch radius
-    patch_radius = IntProperty(name="Patch Radius", default=1, min=1)
-    search_window_radius = IntProperty(name="Search Window Radius", default=6, min=1,
+    patch_radius = IntProperty(name="Patch Radius", default=1, min=1, soft_max=3)
+    search_window_radius = IntProperty(name="Search Window Radius", default=6, min=1, soft_max=9,
                                        description=SEARCH_WINDOW_RADIUS_DESC)
     filter_spikes = BoolProperty(name="Remove Fireflies", default=False,
                                  description=FILTER_SPIKES_DESC)
