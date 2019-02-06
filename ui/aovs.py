163c6a93db257a35777666f914f91b5b20b5917a
Simon Wendsche <simon.wendsche@gmx.de>
2019-02-06 11:48:39 +0100

    update AOV UI because Bidir supports more AOVs now

--- ui/aovs.py
+++ ui/aovs.py
@@ -2,11 +2,6 @@ from bl_ui.properties_render_layer import RenderLayerButtonsPanel
 from bpy.types import Panel
 from ..ui import icons
 
-COMPATIBLE_WITH_BIDIR = {
-    "RADIANCE_GROUP", "SAMPLECOUNT",
-    "ALPHA", "RGBA", "RGB", "DEPTH"
-}
-
 
 class LUXCORE_RENDERLAYER_PT_aovs(RenderLayerButtonsPanel, Panel):
     bl_label = "LuxCore Arbitrary Output Variables (AOVs)"
@@ -25,6 +20,7 @@ class LUXCORE_RENDERLAYER_PT_aovs(RenderLayerButtonsPanel, Panel):
         split = layout.split()
         col = split.column(align=True)
 
+        # Supported by BIDIR
         col.label("Basic Information")
         col.prop(aovs, "rgb")
         col.prop(aovs, "rgba")
@@ -32,19 +28,22 @@ class LUXCORE_RENDERLAYER_PT_aovs(RenderLayerButtonsPanel, Panel):
         col.prop(aovs, "depth")
         col.prop(aovs, "albedo")
 
-        # All following AOVs are not supported by BIDIR, only by PATH
+        # Supported by BIDIR
+        col.label("Material/Object Information")
+        col.prop(aovs, "material_id")
+        col.prop(aovs, "material_id_color")
+        col.prop(aovs, "object_id")
+        # Not supported by BIDIR
         sub = col.column(align=True)
         sub.active = engine_is_path
-        sub.label("Material/Object Information")
-        sub.prop(aovs, "material_id")
-        sub.prop(aovs, "material_id_color")
-        sub.prop(aovs, "object_id")
         sub.prop(aovs, "emission")
 
+        # Not supported by BIDIR
         sub.label("Direct Light Information")
         sub.prop(aovs, "direct_diffuse")
         sub.prop(aovs, "direct_glossy")
 
+        # Not supported by BIDIR
         sub.label("Indirect Light Information")
         sub.prop(aovs, "indirect_diffuse")
         sub.prop(aovs, "indirect_glossy")
@@ -52,24 +51,26 @@ class LUXCORE_RENDERLAYER_PT_aovs(RenderLayerButtonsPanel, Panel):
 
         col = split.column(align=True)
 
+        # Supported by BIDIR
+        col.label("Geometry Information")
+        col.prop(aovs, "position")
+        col.prop(aovs, "shading_normal")
+        col.prop(aovs, "avg_shading_normal")
+        col.prop(aovs, "geometry_normal")
+        col.prop(aovs, "uv")
+
+        # Not supported by BIDIR
         sub = col.column(align=True)
         sub.active = engine_is_path
-
-        sub.label("Geometry Information")
-        sub.prop(aovs, "position")
-        sub.prop(aovs, "shading_normal")
-        sub.prop(aovs, "avg_shading_normal")
-        sub.prop(aovs, "geometry_normal")
-        sub.prop(aovs, "uv")
-
         sub.label("Shadow Information")
         sub.prop(aovs, "direct_shadow_mask")
         sub.prop(aovs, "indirect_shadow_mask")
 
+        # Not supported by BIDIR
         sub.label("Render Information")
         sub.prop(aovs, "irradiance")
         sub.prop(aovs, "raycount")
         sub.prop(aovs, "convergence")
 
-        # Samplecount is supported by BIDIR again
+        # Supported by BIDIR
         col.prop(aovs, "samplecount")
