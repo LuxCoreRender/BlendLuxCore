f8e7e67163326b458d011d4bf465d332299a0ac0
Simon Wendsche <simon.wendsche@gmx.de>
2019-03-01 19:39:29 +0100

    improve PhotonGi UI a bit

--- ui/photongi.py
+++ ui/photongi.py
@@ -17,42 +17,45 @@ class LUXCORE_RENDER_PT_photongi(RenderButtonsPanel, Panel):
     def draw(self, context):
         layout = self.layout
         photongi = context.scene.luxcore.config.photongi
-        layout.active = photongi.enabled
+        engine_is_bidir = context.scene.luxcore.config.engine == "BIDIR"
 
-        if context.scene.luxcore.config.engine == "BIDIR":
+        if engine_is_bidir:
             layout.label(text="Not supported by Bidir", icon=icons.INFO)
 
         if not photongi.indirect_enabled and not photongi.caustic_enabled:
             layout.label(text="All caches disabled", icon=icons.WARNING)
 
-        row = layout.row(align=True)
+        col = layout.column()
+        col.active = not engine_is_bidir
+
+        row = col.row(align=True)
         row.prop(photongi, "photon_maxcount")
         row.prop(photongi, "photon_maxdepth")
 
-        col = layout.column(align=True)
-        col.prop(photongi, "indirect_enabled")
-        col = col.column(align=True)
-        col.active = photongi.indirect_enabled
-        col.prop(photongi, "indirect_maxsize")
-        col.prop(photongi, "indirect_lookup_radius")
-        col.prop(photongi, "indirect_normalangle")
-        col.prop(photongi, "indirect_usagethresholdscale")
-        col.prop(photongi, "indirect_glossinessusagethreshold")
+        sub = col.column(align=True)
+        sub.prop(photongi, "indirect_enabled")
+        sub = sub.column(align=True)
+        sub.active = photongi.indirect_enabled
+        sub.prop(photongi, "indirect_maxsize")
+        sub.prop(photongi, "indirect_lookup_radius")
+        sub.prop(photongi, "indirect_normalangle")
+        sub.prop(photongi, "indirect_usagethresholdscale")
+        sub.prop(photongi, "indirect_glossinessusagethreshold")
 
-        col = layout.column(align=True)
-        col.prop(photongi, "caustic_enabled")
-        row = col.row(align=True)
+        sub = col.column(align=True)
+        sub.prop(photongi, "caustic_enabled")
+        row = sub.row(align=True)
         row.active = photongi.caustic_enabled
         row.prop(photongi, "caustic_maxsize")
-        row = col.row(align=True)
+        row = sub.row(align=True)
         row.active = photongi.caustic_enabled
         row.prop(photongi, "caustic_lookup_radius")
         row.prop(photongi, "caustic_lookup_maxcount")
-        row = col.row(align=True)
+        row = sub.row(align=True)
         row.active = photongi.caustic_enabled
         row.prop(photongi, "caustic_normalangle")
 
-        layout.prop(photongi, "debug")
+        col.prop(photongi, "debug")
         if (photongi.debug == "showindirect" and not photongi.indirect_enabled) or (
                 photongi.debug == "showcaustic" and not photongi.caustic_enabled):
-            layout.label(text="Can't show this cache (disabled)", icon=icons.WARNING)
+            col.label(text="Can't show this cache (disabled)", icon=icons.WARNING)
