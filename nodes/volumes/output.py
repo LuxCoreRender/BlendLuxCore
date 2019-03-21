755c30c4d3d87a5fc90c5472043cc49e742b7a5d
Simon Wendsche <simon.wendsche@gmx.de>
2019-03-21 18:51:09 +0100

    make tooltip of volume use_photongi property more clear, disable the prop for clear volumes

--- nodes/volumes/output.py
+++ nodes/volumes/output.py
@@ -15,9 +15,11 @@ class LuxCoreNodeVolOutput(LuxCoreNodeOutput):
 
     active = BoolProperty(name="Active", default=True, update=update_active)
     use_photongi = BoolProperty(name="Use PhotonGI Cache", default=True,
-                                description="Store PhotonGI entries in this volume. You might want to "
-                                            "disable this for volumes that take up a lot of space while "
-                                            "having low scattering, like an air volume in a large open scene")
+                                description="Store PhotonGI entries in this volume. This only affects "
+                                            "homogeneous and heterogeneous volumes, entries are never "
+                                            "stored on clear volumes. You might want to disable this "
+                                            "for volumes that take up a lot of space while having low "
+                                            "scattering, like a fog volume in a large open scene")
 
     def init(self, context):
         self.inputs.new("LuxCoreSocketVolume", "Volume")
@@ -29,7 +31,12 @@ class LuxCoreNodeVolOutput(LuxCoreNodeOutput):
         # PhotonGI currently only works with Path engine
         if (context.scene.luxcore.config.photongi.enabled
                 and context.scene.luxcore.config.engine == "PATH"):
-            layout.prop(self, "use_photongi")
+            # PhotonGI only affects homogeneous and heterogeneous volumes, make the setting inactive for others
+            linked_node = self.inputs["Volume"].links[0].from_node if self.inputs["Volume"].is_linked else False
+            row = layout.row()
+            row.active = linked_node and linked_node.bl_idname in {"LuxCoreNodeVolHomogeneous",
+                                                                   "LuxCoreNodeVolHeterogeneous"}
+            row.prop(self, "use_photongi")
 
     def export(self, exporter, props, luxcore_name):
         # Invalidate node cache
