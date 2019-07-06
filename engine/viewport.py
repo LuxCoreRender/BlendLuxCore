LuxCoreRender/BlendLuxCore

commit 17ab8b084d2cbe72dba51e3b1797d45ce40007fa

make errorlog a singleton instead of a PropertyGroup

engine/viewport.py
@@ -2,6 +2,7 @@ from time import time
 from .. import export
 from ..draw.viewport import FrameBuffer
 from ..utils import render as utils_render
+from ..utils.errorlog import LuxCoreErrorLog
 
 import bpy
 from ..bin import pyluxcore
@@ -22,7 +23,7 @@ def start_session(engine):
         engine.exporter = None
 
         engine.update_stats("Error: ", str(error))
-        # scene.luxcore.errorlog.add_error(error)  # TODO 2.8
+        LuxCoreErrorLog.add_error(error)
 
         import traceback
         traceback.print_exc()
@@ -42,7 +43,7 @@ def view_update(engine, context, depsgraph, changes=None):
     if engine.framebuffer:
         engine.framebuffer.reset_denoiser()
 
-    #scene.luxcore.errorlog.clear()  # TODO 2.8
+        LuxCoreErrorLog.clear()  # TODO 2.8
 
     if engine.session is None:
         print("=" * 50)
@@ -62,7 +63,7 @@ def view_update(engine, context, depsgraph, changes=None):
             engine.exporter = None
 
             engine.update_stats("Error: ", str(error))
-            # scene.luxcore.errorlog.add_error(error)  # TODO 2.8
+            LuxCoreErrorLog.add_error(error)
 
             import traceback
             traceback.print_exc()

engine/base.py
@@ -3,6 +3,7 @@ from time import sleep
 from . import final, preview, viewport
 from ..handlers.draw_imageeditor import TileStats
 from ..utils.log import LuxCoreLog
+from ..utils.errorlog import LuxCoreErrorLog
 
 
 class LuxCoreRenderEngine(bpy.types.RenderEngine):
@@ -87,7 +88,7 @@ class LuxCoreRenderEngine(bpy.types.RenderEngine):
             import traceback
             traceback.print_exc()
             # Add error to error log so the user can inspect and copy/paste it
-            scene.luxcore.errorlog.add_error(error)
+            LuxCoreErrorLog.add_error(error)
 
             # Clean up
             del self.session

engine/final.py
@@ -2,11 +2,12 @@ from time import time, sleep
 from .. import export, utils
 from ..draw.final import FrameBufferFinal
 from ..utils import render as utils_render
+from ..utils.errorlog import LuxCoreErrorLog
 
 
 def render(engine, scene):
     print("=" * 50)
-    scene.luxcore.errorlog.clear()
+    LuxCoreErrorLog.clear()
     scene.luxcore.denoiser_log.clear()
     render_slot_stats = scene.luxcore.statistics.get_active()
 
@@ -15,7 +16,7 @@ def render(engine, scene):
         if len(scene.render.layers) > 1 and tonemapper.is_automatic():
             msg = ("Using an automatic tonemapper with multiple "
                    "renderlayers will result in brightness differences")
-            scene.luxcore.errorlog.add_warning(msg)
+            LuxCoreErrorLog.add_warning(msg)
 
     _check_halt_conditions(engine, scene)
 
@@ -211,8 +212,7 @@ def _check_halt_conditions(engine, scene):
                 is_halt_enabled &= has_halt_condition
 
                 if not has_halt_condition:
-                    msg = 'Halt condition missing for render layer "%s"' % layer.name
-                    scene.luxcore.errorlog.add_error(msg)
+                    LuxCoreErrorLog.add_error('Halt condition missing for render layer "%s"' % layer.name)
             else:
                 is_halt_enabled = False
 

export/__init__.py
@@ -4,6 +4,7 @@ from ..bin import pyluxcore
 from .. import utils
 from ..utils import render as utils_render
 from ..utils import compatibility as utils_compatibility
+from ..utils.errorlog import LuxCoreErrorLog
 from . import (
     blender_object, caches, camera, config, duplis,
     group_instance, imagepipeline, light, material,
@@ -171,7 +172,7 @@ class Exporter(object):
         light_count = luxcore_scene.GetLightCount()
         if light_count > 1000:
             msg = "The scene contains a lot of light sources (%d), performance might suffer" % light_count
-            scene.luxcore.errorlog.add_warning(msg)
+            LuxCoreErrorLog.add_warning(msg)
         if stats:
             stats.light_count.value = light_count
 
@@ -266,14 +267,14 @@ class Exporter(object):
                 props = self._update_scene(depsgraph, context, changes, luxcore_scene)
                 luxcore_scene.Parse(props)
             except Exception as error:
-                context.scene.luxcore.errorlog.add_error(error)
+                LuxCoreErrorLog.add_error(error)
                 import traceback
                 traceback.print_exc()
 
             try:
                 session.EndSceneEdit()
             except RuntimeError as error:
-                context.scene.luxcore.errorlog.add_error(error)
+                LuxCoreErrorLog.add_error(error)
                 # Probably no light source, save ourselves by adding one (otherwise a crash happens)
                 props = pyluxcore.Properties()
                 props.Set(pyluxcore.Property("scene.lights.__SAVIOR__.type", "constantinfinite"))

export/aovs.py
@@ -3,6 +3,7 @@ from ..bin import pyluxcore
 from .. import utils
 from . import imagepipeline
 from .imagepipeline import use_backgroundimage
+from ..utils.errorlog import LuxCoreErrorLog
 
 # set of channels that don"t use an HDR format
 LDR_CHANNELS = {
@@ -111,7 +112,7 @@ def convert(exporter, scene, context=None, engine=None):
                                                      group_id, exporter.lightgroup_cache)
 
             if not any([group.enabled for group in scene.luxcore.lightgroups.get_all_groups()]):
-                scene.luxcore.errorlog.add_warning("All light groups are disabled.")
+                LuxCoreErrorLog.add_warning("All light groups are disabled.")
 
             # Denoiser imagepipeline
             if scene.luxcore.denoiser.enabled:
@@ -125,8 +126,7 @@ def convert(exporter, scene, context=None, engine=None):
     except Exception as error:
         import traceback
         traceback.print_exc()
-        msg = "AOVs: %s" % error
-        scene.luxcore.errorlog.add_warning(msg)
+        LuxCoreErrorLog.add_warning("AOVs: %s" % error)
         return pyluxcore.Properties()
 
 
@@ -172,8 +172,7 @@ def _make_imagepipeline(props, context, scene, output_name, pipeline_index, outp
     if tonemapper.is_automatic():
         # We can not work with an automatic tonemapper because
         # every AOV will differ in brightness
-        msg = "Use a non-automatic tonemapper to get tonemapped AOVs"
-        scene.luxcore.errorlog.add_warning(msg)
+        LuxCoreErrorLog.add_warning("Use a non-automatic tonemapper to get tonemapped AOVs")
         return pipeline_index
 
     prefix = "film.imagepipelines." + str(pipeline_index) + "."

export/blender_object.py
@@ -89,7 +89,7 @@ def convert(exporter, obj, scene, context, luxcore_scene,
         return props, ExportedObject(mesh_definitions, luxcore_name)
     except Exception as error:
         msg = 'Object "%s": %s' % (obj.name, error)
-        scene.luxcore.errorlog.add_warning(msg)
+        LuxCoreErrorLog.add_warning(msg)
         import traceback
         traceback.print_exc()
         return pyluxcore.Properties(), None
@@ -111,11 +111,11 @@ def define_from_mesh_defs(mesh_definitions, scene, context, exporter, obj, props
                 if mat is None:
                     # Note: material.convert returns the fallback material in this case
                     msg = 'Object "%s": No material attached to slot %d' % (obj.name, material_index)
-                    scene.luxcore.errorlog.add_warning(msg, obj_name=obj.name)
+                    LuxCoreErrorLog.add_warning(msg, obj_name=obj.name)
             else:
                 # The object has no material slots
                 msg = 'Object "%s": No material defined' % obj.name
-                scene.luxcore.errorlog.add_warning(msg, obj_name=obj.name)
+                LuxCoreErrorLog.add_warning(msg, obj_name=obj.name)
                 # Use fallback material
                 mat = None
 
@@ -125,7 +125,7 @@ def define_from_mesh_defs(mesh_definitions, scene, context, exporter, obj, props
                 if imagemaps and not utils_node.has_valid_uv_map(obj):
                     msg = ('Object "%s": %d image texture(s) used, but no UVs defined. ' 
                            'In case of bumpmaps this can lead to artifacts' % (obj.name, len(imagemaps)))
-                    scene.luxcore.errorlog.add_warning(msg, obj_name=obj.name)
+                    LuxCoreErrorLog.add_warning(msg, obj_name=obj.name)
 
             lux_mat_name, mat_props = material.convert(exporter, mat, scene, context, obj.name)
         else:

export/caches/object_cache.py
@@ -14,13 +14,14 @@ class ObjectCache2:
         self.exported_meshes = {}
 
     def first_run(self, exporter, depsgraph, engine, luxcore_scene, scene_props, is_viewport_render):
+        # TODO use luxcore_scene.DuplicateObjects for instances
         for index, dg_obj_instance in enumerate(depsgraph.object_instances, start=1):
             obj = dg_obj_instance.instance_object if dg_obj_instance.is_instance else dg_obj_instance.object
             if not self._is_visible(dg_obj_instance, obj):
                 continue
 
-            self._convert(exporter, dg_obj_instance, obj, depsgraph,
-                          luxcore_scene, scene_props, is_viewport_render)
+            self._convert_obj(exporter, dg_obj_instance, obj, depsgraph,
+                              luxcore_scene, scene_props, is_viewport_render)
             if engine:
                 # Objects are the most expensive to export, so they dictate the progress
                 # engine.update_progress(index / obj_amount)
@@ -46,19 +47,16 @@ class ObjectCache2:
         # Important: we need the data of the original object, not the evaluated one
         return utils.get_luxcore_name(obj.original.data, is_viewport_render)
 
-    def _convert(self, exporter, dg_obj_instance, obj, depsgraph, luxcore_scene, scene_props, is_viewport_render):
+    def _convert_obj(self, exporter, dg_obj_instance, obj, depsgraph, luxcore_scene, scene_props, is_viewport_render):
         """ Convert one DepsgraphObjectInstance amd keep track of it """
+        if obj.type == "EMPTY" or obj.data is None:
+            return
+
         obj_key = utils.make_key_from_instance(dg_obj_instance)
 
-        if obj.type == "EMPTY" or obj.data is None:
-            # Not sure if we even need a special empty export, could just ignore them
-            #print("empty export not implemented yet")
-            pass
-        elif obj.type in MESH_OBJECTS:
+        if obj.type in MESH_OBJECTS:
             if obj_key in self.exported_objects:
                 raise Exception("key already in exp_obj:", obj_key)
-
-            print("converting mesh object", obj.name_full)
             self._convert_mesh_obj(dg_obj_instance, obj, obj_key, depsgraph, luxcore_scene,
                                    scene_props, is_viewport_render)
         elif obj.type == "LIGHT":
@@ -120,8 +118,9 @@ class ObjectCache2:
                     scene_props.Set(exported_obj.get_props())
             else:
                 # Object is new and not in LuxCore yet, or it is a light, do a full export
-                self._convert(exporter, dg_obj_instance, obj, depsgraph,
-                              luxcore_scene, scene_props, is_viewport_render)
+                # TODO use luxcore_scene.DuplicateObjects for instances
+                self._convert_obj(exporter, dg_obj_instance, obj, depsgraph,
+                                  luxcore_scene, scene_props, is_viewport_render)
 
         # Geometry updates (mesh edit, modifier edit etc.)
         if depsgraph.id_type_updated("OBJECT"):

export/camera.py
@@ -3,6 +3,7 @@ from mathutils import Vector, Matrix
 from ..bin import pyluxcore
 from .. import utils
 from ..nodes.output import get_active_output
+from ..utils.errorlog import LuxCoreErrorLog
 
 
 def convert(exporter, scene, context=None, is_camera_moving=False):
@@ -183,7 +184,7 @@ def _clipping(scene, definitions):
 
         if warning:
             msg = 'Camera: %s' % warning
-            scene.luxcore.errorlog.add_warning(msg, obj_name=camera.name)
+            LuxCoreErrorLog.add_warning(msg, obj_name=camera.name)
 
 
 def _clipping_plane(scene, definitions):
@@ -258,7 +259,7 @@ def _get_volume_props(exporter, scene):
             props.Set(pyluxcore.Property("scene.camera.volume", luxcore_name))
         except Exception as error:
             msg = 'Camera: %s' % error
-            scene.luxcore.errorlog.add_warning(msg, obj_name=scene.camera.name)
+            LuxCoreErrorLog.add_warning(msg, obj_name=scene.camera.name)
 
     props.Set(pyluxcore.Property("scene.camera.autovolume.enable", cam_settings.auto_volume))
     return props

commit 09bac44d24ec8ccbf96d667213d661472fcab31f

fix bug introduced in 17ab8b084d2cbe72dba51e3b1797d45ce40007fa

engine/viewport.py
@@ -43,7 +43,7 @@ def view_update(engine, context, depsgraph, changes=None):
     if engine.framebuffer:
         engine.framebuffer.reset_denoiser()
 
-        LuxCoreErrorLog.clear()  # TODO 2.8
+    LuxCoreErrorLog.clear()
 
     if engine.session is None:
         print("=" * 50)