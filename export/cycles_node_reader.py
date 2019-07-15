b4c96f39db9a90f7dd9c6855d1a7831f7002d60a
Simon Wendsche <simon.wendsche@gmx.de>
2019-07-15 13:58:57 +0200

    cycles node reader: basic node group support

--- export/cycles_node_reader.py
+++ export/cycles_node_reader.py
@@ -6,6 +6,7 @@ ERROR_VALUE = 0
 
 
 def convert(material, props, luxcore_name, obj_name=""):
+    print("Converting Cycles node tree of material", material.name_full)
     output = material.node_tree.get_output_node("CYCLES")
     if output is None or not output.inputs["Surface"].is_linked:
         return black(luxcore_name)
@@ -13,6 +14,7 @@ def convert(material, props, luxcore_name, obj_name=""):
     result = _node(link.from_node, link.from_socket, props, luxcore_name, obj_name)
     if result == 0:
         return black(luxcore_name)
+    assert result == luxcore_name
     return luxcore_name, props
 
 
@@ -25,11 +27,11 @@ def black(luxcore_name="__BLACK__"):
     return luxcore_name, props
 
 
-def _socket(socket, props, obj_name=""):
+def _socket(socket, props, obj_name, group_node):
     # TODO use more advanced functions from our own socket base class (bypass reroutes etc.)
     if socket.is_linked:
         link = socket.links[0]
-        return _node(link.from_node, link.from_socket, props, obj_name=obj_name)
+        return _node(link.from_node, link.from_socket, props, None, obj_name, group_node)
     else:
         if not hasattr(socket, "default_value"):
             return ERROR_VALUE
@@ -41,37 +43,40 @@ def _socket(socket, props, obj_name=""):
             return socket.default_value
 
 
-def _node(node, output_socket, props, luxcore_name=None, obj_name=""):
+def _node(node, output_socket, props, luxcore_name=None, obj_name="", group_node=None):
     if luxcore_name is None:
-        luxcore_name = str(node.as_pointer()) + utils.sanitize_luxcore_name(output_socket.name)
+        luxcore_name = str(node.as_pointer()) + output_socket.name
+        if group_node:
+            luxcore_name += str(group_node.as_pointer())
+        luxcore_name = utils.sanitize_luxcore_name(luxcore_name)
 
     if node.bl_idname == "ShaderNodeBsdfPrincipled":
         prefix = "scene.materials."
         definitions = {
             "type": "disney",
-            "basecolor": _socket(node.inputs["Base Color"], props, obj_name),
+            "basecolor": _socket(node.inputs["Base Color"], props, obj_name, group_node),
             "subsurface": 0,  # TODO
-            "metallic": _socket(node.inputs["Metallic"], props, obj_name),
-            "specular": _socket(node.inputs["Specular"], props, obj_name),
-            "speculartint": _socket(node.inputs["Specular Tint"], props, obj_name),
-            "roughness": _socket(node.inputs["Roughness"], props, obj_name),
-            "anisotropic": _socket(node.inputs["Anisotropic"], props, obj_name),
-            "sheen": _socket(node.inputs["Sheen"], props, obj_name),
-            "sheentint": _socket(node.inputs["Sheen Tint"], props, obj_name),
-            "clearcoat": _socket(node.inputs["Clearcoat"], props, obj_name),
-            #"clearcoatgloss": convert_cycles_socket(node.inputs["Clearcoat Roughness"], props, obj_name),  # TODO
+            "metallic": _socket(node.inputs["Metallic"], props, obj_name, group_node),
+            "specular": _socket(node.inputs["Specular"], props, obj_name, group_node),
+            "speculartint": _socket(node.inputs["Specular Tint"], props, obj_name, group_node),
+            "roughness": _socket(node.inputs["Roughness"], props, obj_name, group_node),
+            "anisotropic": _socket(node.inputs["Anisotropic"], props, obj_name, group_node),
+            "sheen": _socket(node.inputs["Sheen"], props, obj_name, group_node),
+            "sheentint": _socket(node.inputs["Sheen Tint"], props, obj_name, group_node),
+            "clearcoat": _socket(node.inputs["Clearcoat"], props, obj_name, group_node),
+            #"clearcoatgloss": convert_cycles_socket(node.inputs["Clearcoat Roughness"], props, obj_name, group_node),  # TODO
         }
     elif node.bl_idname == "ShaderNodeMixShader":
         prefix = "scene.materials."
 
         def convert_mat_socket(index):
-            mat_name = _socket(node.inputs[index], props, obj_name)
+            mat_name = _socket(node.inputs[index], props, obj_name, group_node)
             if mat_name == ERROR_VALUE:
                 mat_name, mat_props = black()
                 props.Set(mat_props)
             return mat_name
 
-        amount = _socket(node.inputs["Fac"], props, obj_name)
+        amount = _socket(node.inputs["Fac"], props, obj_name, group_node)
 
         definitions = {
             "type": "mix",
@@ -84,7 +89,7 @@ def _node(node, output_socket, props, luxcore_name=None, obj_name=""):
         # TODO roughmatte and roughness -> sigma conversion (if possible)
         definitions = {
             "type": "matte",
-            "kd": _socket(node.inputs["Color"], props, obj_name),
+            "kd": _socket(node.inputs["Color"], props, obj_name, group_node),
         }
     elif node.bl_idname == "ShaderNodeBsdfGlossy":
         prefix = "scene.materials."
@@ -94,11 +99,11 @@ def _node(node, output_socket, props, luxcore_name=None, obj_name=""):
         helper_prefix = "scene.textures." + tex_name + "."
         helper_defs = {
             "type": "fresnelcolor",
-            "kr": _socket(node.inputs["Color"], props, obj_name),
+            "kr": _socket(node.inputs["Color"], props, obj_name, group_node),
         }
         props.Set(utils.create_props(helper_prefix, helper_defs))
 
-        roughness = _socket(node.inputs["Roughness"], props, obj_name)
+        roughness = _socket(node.inputs["Roughness"], props, obj_name, group_node)
 
         definitions = {
             "type": "metal2",
@@ -134,19 +139,37 @@ def _node(node, output_socket, props, luxcore_name=None, obj_name=""):
             return [1, 0, 1]
     elif node.bl_idname == "ShaderNodeBsdfGlass":
         prefix = "scene.materials."
-        color = _socket(node.inputs["Color"], props, obj_name)
-        roughness = _socket(node.inputs["Roughness"], props, obj_name)
+        color = _socket(node.inputs["Color"], props, obj_name, group_node)
+        roughness = _socket(node.inputs["Roughness"], props, obj_name, group_node)
 
         definitions = {
             "type": "glass" if roughness == 0 else "roughglass",
             "kt": color,
             "kr": color, # Nonsense, maybe leave white even if it breaks compatibility with Cycles?
-            "interiorior": _socket(node.inputs["IOR"], props, obj_name),
+            "interiorior": _socket(node.inputs["IOR"], props, obj_name, group_node),
         }
 
         if roughness != 0:
             definitions["uroughness"] = roughness
             definitions["vroughness"] = roughness
+    elif node.bl_idname == "ShaderNodeGroup":
+        active_output = None
+        for subnode in node.node_tree.nodes:
+            if subnode.bl_idname == "NodeGroupOutput" and subnode.is_active_output:
+                active_output = subnode
+                break
+
+        current_input = active_output.inputs[output_socket.name]
+        if not current_input.is_linked:
+            return ERROR_VALUE
+
+        link = current_input.links[0]
+        # I call _node instead of _socket here because I need to pass the
+        # luxcore_name in case the node group is the first node in the tree
+        return _node(link.from_node, link.from_socket, props, luxcore_name, obj_name, node)
+    elif node.bl_idname == "NodeGroupInput":
+        # TODO I set group_node to None, but what about nested groups?
+        return _socket(group_node.inputs[output_socket.name], props, obj_name, None)
     else:
         LuxCoreErrorLog.add_warning(f"Unknown node type: {node.name}", obj_name=obj_name)
         return ERROR_VALUE
