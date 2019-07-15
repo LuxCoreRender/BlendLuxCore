220e3d9f2379abfc69eae05f7ed61031b9439d67
Simon Wendsche <simon.wendsche@gmx.de>
2019-07-15 12:46:03 +0200

    some simplification in cycles node reader

--- export/cycles_node_reader.py
+++ export/cycles_node_reader.py
@@ -10,7 +10,7 @@ def convert(material, props, luxcore_name, obj_name=""):
     if output is None or not output.inputs["Surface"].is_linked:
         return black(luxcore_name)
     link = output.inputs["Surface"].links[0]
-    result = convert_cycles_node(link.from_node, link.from_socket, props, luxcore_name, obj_name)
+    result = _node(link.from_node, link.from_socket, props, luxcore_name, obj_name)
     if result == 0:
         return black(luxcore_name)
     return luxcore_name, props
@@ -25,11 +25,11 @@ def black(luxcore_name="__BLACK__"):
     return luxcore_name, props
 
 
-def convert_cycles_socket(socket, props, obj_name=""):
+def _socket(socket, props, obj_name=""):
     # TODO use more advanced functions from our own socket base class (bypass reroutes etc.)
     if socket.is_linked:
         link = socket.links[0]
-        return convert_cycles_node(link.from_node, link.from_socket, props, obj_name=obj_name)
+        return _node(link.from_node, link.from_socket, props, obj_name=obj_name)
     else:
         if not hasattr(socket, "default_value"):
             return ERROR_VALUE
@@ -41,7 +41,7 @@ def convert_cycles_socket(socket, props, obj_name=""):
             return socket.default_value
 
 
-def convert_cycles_node(node, output_socket, props, luxcore_name=None, obj_name=""):
+def _node(node, output_socket, props, luxcore_name=None, obj_name=""):
     if luxcore_name is None:
         luxcore_name = str(node.as_pointer()) + utils.sanitize_luxcore_name(output_socket.name)
 
@@ -49,29 +49,29 @@ def convert_cycles_node(node, output_socket, props, luxcore_name=None, obj_name=
         prefix = "scene.materials."
         definitions = {
             "type": "disney",
-            "basecolor": convert_cycles_socket(node.inputs["Base Color"], props, obj_name),
-            "subsurface": 0,
-            "metallic": convert_cycles_socket(node.inputs["Metallic"], props, obj_name),
-            "specular": convert_cycles_socket(node.inputs["Specular"], props, obj_name),
-            "speculartint": convert_cycles_socket(node.inputs["Specular Tint"], props, obj_name),
-            "roughness": convert_cycles_socket(node.inputs["Roughness"], props, obj_name),
-            "anisotropic": convert_cycles_socket(node.inputs["Anisotropic"], props, obj_name),
-            "sheen": convert_cycles_socket(node.inputs["Sheen"], props, obj_name),
-            "sheentint": convert_cycles_socket(node.inputs["Sheen Tint"], props, obj_name),
-            "clearcoat": convert_cycles_socket(node.inputs["Clearcoat"], props, obj_name),
-            #"clearcoatgloss": convert_cycles_socket(node.inputs["Clearcoat Roughness"], props, obj_name), # TODO
+            "basecolor": _socket(node.inputs["Base Color"], props, obj_name),
+            "subsurface": 0,  # TODO
+            "metallic": _socket(node.inputs["Metallic"], props, obj_name),
+            "specular": _socket(node.inputs["Specular"], props, obj_name),
+            "speculartint": _socket(node.inputs["Specular Tint"], props, obj_name),
+            "roughness": _socket(node.inputs["Roughness"], props, obj_name),
+            "anisotropic": _socket(node.inputs["Anisotropic"], props, obj_name),
+            "sheen": _socket(node.inputs["Sheen"], props, obj_name),
+            "sheentint": _socket(node.inputs["Sheen Tint"], props, obj_name),
+            "clearcoat": _socket(node.inputs["Clearcoat"], props, obj_name),
+            #"clearcoatgloss": convert_cycles_socket(node.inputs["Clearcoat Roughness"], props, obj_name),  # TODO
         }
     elif node.bl_idname == "ShaderNodeMixShader":
         prefix = "scene.materials."
 
         def convert_mat_socket(index):
-            mat_name = convert_cycles_socket(node.inputs[index], props, obj_name)
+            mat_name = _socket(node.inputs[index], props, obj_name)
             if mat_name == ERROR_VALUE:
                 mat_name, mat_props = black()
                 props.Set(mat_props)
             return mat_name
 
-        amount = convert_cycles_socket(node.inputs["Fac"], props, obj_name)
+        amount = _socket(node.inputs["Fac"], props, obj_name)
 
         definitions = {
             "type": "mix",
@@ -84,7 +84,7 @@ def convert_cycles_node(node, output_socket, props, luxcore_name=None, obj_name=
         # TODO roughmatte and roughness -> sigma conversion (if possible)
         definitions = {
             "type": "matte",
-            "kd": convert_cycles_socket(node.inputs["Color"], props, obj_name),
+            "kd": _socket(node.inputs["Color"], props, obj_name),
         }
     elif node.bl_idname == "ShaderNodeBsdfGlossy":
         prefix = "scene.materials."
@@ -94,11 +94,11 @@ def convert_cycles_node(node, output_socket, props, luxcore_name=None, obj_name=
         helper_prefix = "scene.textures." + tex_name + "."
         helper_defs = {
             "type": "fresnelcolor",
-            "kr": convert_cycles_socket(node.inputs["Color"], props, obj_name),
+            "kr": _socket(node.inputs["Color"], props, obj_name),
         }
         props.Set(utils.create_props(helper_prefix, helper_defs))
 
-        roughness = convert_cycles_socket(node.inputs["Roughness"], props, obj_name)
+        roughness = _socket(node.inputs["Roughness"], props, obj_name)
 
         definitions = {
             "type": "metal2",
@@ -134,14 +134,14 @@ def convert_cycles_node(node, output_socket, props, luxcore_name=None, obj_name=
             return [1, 0, 1]
     elif node.bl_idname == "ShaderNodeBsdfGlass":
         prefix = "scene.materials."
-        color = convert_cycles_socket(node.inputs["Color"], props, obj_name)
-        roughness = convert_cycles_socket(node.inputs["Roughness"], props, obj_name)
+        color = _socket(node.inputs["Color"], props, obj_name)
+        roughness = _socket(node.inputs["Roughness"], props, obj_name)
 
         definitions = {
             "type": "glass" if roughness == 0 else "roughglass",
             "kt": color,
             "kr": color, # Nonsense, maybe leave white even if it breaks compatibility with Cycles?
-            "interiorior": convert_cycles_socket(node.inputs["IOR"], props, obj_name),
+            "interiorior": _socket(node.inputs["IOR"], props, obj_name),
         }
 
         if roughness != 0:
