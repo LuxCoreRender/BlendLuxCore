eb5572f8dc30dbffa214b79f5c18482eddabc100
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-09-09 22:34:26 +0200

    Testing final release

--- scripts/azurepipeline/package.sh
+++ scripts/azurepipeline/package.sh
@@ -5,4 +5,3 @@ python3 ./package_releases.py $VERSION_STRING
 
 # cp ./release-$VERSION_STRING/* $BUILD_ARTIFACTSTAGINGDIRECTORY
 # cp ./release-$VERSION_STRING-blender2.80/* $BUILD_ARTIFACTSTAGINGDIRECTORY
-
