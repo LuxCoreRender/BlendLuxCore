d23ab3315e1b56e35d49e257cf753d784f92836c
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2019-10-09 10:25:47 +0200

    Disable Blender 2.79b support, now only 2.8x compatible versions are produced

--- scripts/azurepipeline/package.sh
+++ scripts/azurepipeline/package.sh
@@ -4,4 +4,3 @@ cd release
 python3 ./package_releases.py $VERSION_STRING
 
 cp ./release-$VERSION_STRING/* $BUILD_ARTIFACTSTAGINGDIRECTORY
-cp ./release-$VERSION_STRING-blender2.80/* $BUILD_ARTIFACTSTAGINGDIRECTORY
