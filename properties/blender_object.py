890ead617abdbf1332cd512e6d477603f1d23c0c
michelchafouin <michel.chafouin@gmail.com>
2019-12-13 12:11:53 +0900

    Adding Exclude from Render flag to Objects

--- properties/blender_object.py
+++ properties/blender_object.py
@@ -13,10 +13,13 @@ DESC_OBJECT_ID = (
     "Note that the random IDs of LuxCore can be greater than 32767 "
     "(the ID Mask node in the compositor can't handle those numbers)"
 )
-
+DESC_EXCLUDE_FROM_RENDER = ("The object will be excluded from render. "
+    "Useful if you need objects to render for other engines, but not for LuxCore."
+)
 
 class LuxCoreObjectProps(PropertyGroup):
     visible_to_camera: BoolProperty(name="Visible to Camera", default=True, description=DESC_VISIBLE_TO_CAM)
+    exclude_from_render: BoolProperty(name="Exclude from render", default=False, description=DESC_EXCLUDE_FROM_RENDER)
     enable_motion_blur: BoolProperty(name="Motion Blur", default=True, description=DESC_MOTION_BLUR)
     id: IntProperty(name="Object ID", default=-1, min=-1, soft_max=32767, description=DESC_OBJECT_ID)
 
