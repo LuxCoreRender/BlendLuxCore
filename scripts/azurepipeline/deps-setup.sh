149581031da48a83162ff148700104d1aa26b1df
u3dreal <48415446+u3dreal@users.noreply.github.com>
2019-12-26 20:04:16 +0100

    Update deps-setup.sh

--- scripts/azurepipeline/deps-setup.sh
+++ scripts/azurepipeline/deps-setup.sh
@@ -2,4 +2,6 @@
 
 # Install deps
 sudo apt-get -qq update
-sudo apt-get install -y git zip wget bzip2 p7zip-full
+sudo apt-get install -y git zip wget bzip2
+wget http://neurodebian.ovgu.de/debian/pool/main/p/p7zip/p7zip-full_16.02+dfsg-1~nd14.04+1_amd64.deb
+sudo dpkg -i p7zip-full_16.02+dfsg-1~nd14.04+1_amd64.deb
