00d9dcf267798c9fd666be15f83f860562172df4
u3dreal <48415446+u3dreal@users.noreply.github.com>
2019-12-28 02:21:46 +0100

    Update package_releases.py

--- release/package_releases.py
+++ release/package_releases.py
@@ -169,7 +169,7 @@ def extract_files_from_dmg(dmg_path, files_to_extract, destination):
         dst = os.path.join(destination, f)
         print('Moving "%s" to "%s"' % (src, dst))
         shutil.move(src, dst)
-    os.system("chmod 755 " + destination + "denoise")
+    os.system("chmod 755 " + destination + "/denoise")
         
     rmtree(temp_dir)
         
