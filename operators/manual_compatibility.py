9ad101ac651262d4c3b50a1212cd4569211e4e16
Simon Wendsche <simon.wendsche@gmx.de>
2020-02-20 14:14:11 +0100

    trying to fix area light conversion, not finished

--- operators/manual_compatibility.py
+++ operators/manual_compatibility.py
@@ -6,8 +6,12 @@ from ..utils.node import find_nodes
 # TODO:
 #  * The ImagePipeline tonemapper default settings were changed: auto brightness is now disabled by default (was enabled),
 #    gain is now 1.0 by default (was 0.5)
-#  * Lights now have two modes for choosing the brightness: "artistic" and "power". Default is now "artistic". This means
-#    that the light settings of old scenes have to be changed manually to make them look identical to v2.2.
+
+# TODO prevent user from pressing the button twice
+
+def calc_power_correction_factor(spread_angle):
+    return math.pi * (2 * math.pi * (1 - math.cos(spread_angle / 2)))
+
 
 class LUXCORE_OT_convert_to_v23(bpy.types.Operator):
     bl_idname = "luxcore.convert_to_v23"
@@ -39,8 +43,10 @@ class LUXCORE_OT_convert_to_v23(bpy.types.Operator):
                     light.update_tag()
             
             # Converting Area lights so new Power matches 2.2 power per unit angle
-            if light.type == "AREA" and light.luxcore.light_unit == "power":
-                light.luxcore.power *= math.pi * ( 2 * math.pi * (1 - math.cos(light.luxcore.spread_angle/2) )) 
+            if light.type == "AREA":
+                light.luxcore.light_unit = "power"
+                light.luxcore.power *= calc_power_correction_factor(light.luxcore.spread_angle)
+                light.update_tag()
 
         for world in bpy.data.worlds:
             if world.luxcore.light == "sky2":
@@ -60,8 +66,8 @@ class LUXCORE_OT_convert_to_v23(bpy.types.Operator):
             
             # Converting Emissive materials so new Power matches 2.2 power per unit angle
             for emission_node in find_nodes(node_tree, "LuxCoreNodeMatEmission", False):
-                if emission_node.emission_unit == "power":
-                    emission_node.power *= math.pi * ( 2 * math.pi * (1 - math.cos(light.luxcore.spread_angle/2) ))
+                emission_node.emission_unit = "power"
+                emission_node.power *= calc_power_correction_factor(emission_node.spread_angle)
             
 
         return {"FINISHED"}
