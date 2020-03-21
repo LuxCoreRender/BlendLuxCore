20c5fc45906e200296704a93758e0cb5cfb4965c
Simon Wendsche <simon.wendsche@gmx.de>
2020-03-21 14:03:44 +0100

    make viewer shortcut work in texture and volume node trees

--- operators/node_editor.py
+++ operators/node_editor.py
@@ -15,7 +15,7 @@ def _remove_all_viewers(node_tree):
             node_tree.nodes.remove(node)
 
 
-def _get_emission_viewer_gain(context):
+def _calc_emission_viewer_gain(context):
     # Compensate for tonemapping and color management
     gain = 1
     if utils.is_valid_camera(context.scene.camera):
@@ -68,49 +68,56 @@ class LUXCORE_OT_node_editor_viewer(bpy.types.Operator):
         allowed_inputs = set()
         sockets_to_link = []
 
-        if active_node.bl_idname.startswith("LuxCoreNodeMat"):
-            target_socket = active_output.inputs["Material"]
+        if node_tree.bl_idname == "luxcore_material_nodes":
+            if active_node.bl_idname.startswith("LuxCoreNodeMat"):
+                target_socket = active_output.inputs["Material"]
+                allowed_inputs = target_socket.allowed_inputs
+                if viewer_node:
+                    _remove_all_viewers(node_tree)
+            elif active_node.bl_idname.startswith("LuxCoreNodeShape"):
+                target_socket = active_output.inputs["Shape"]
+                allowed_inputs = target_socket.allowed_inputs
+                if viewer_node:
+                    _remove_all_viewers(node_tree)
+            elif active_node.bl_idname.startswith("LuxCoreNodeTex"):
+                if viewer_node:
+                    # Existing viewer setup, find the emission node and update the gain
+                    for node in node_tree.nodes:
+                        if luxcore_viewer_mark in node and node.bl_idname == "LuxCoreNodeMatEmission":
+                            node.gain = _calc_emission_viewer_gain(context)
+                            break
+                else:
+                    viewer_matte = node_tree.nodes.new("LuxCoreNodeMatMatte")
+                    viewer_matte.location = active_output.location + mathutils.Vector((0, 50))
+                    viewer_matte.hide = True
+                    viewer_matte[luxcore_viewer_mark] = True
+                    sockets_to_link.append((viewer_matte.outputs[0], active_output.inputs["Material"]))
+
+                    viewer_emission = node_tree.nodes.new("LuxCoreNodeMatEmission")
+                    viewer_emission.location = active_output.location + mathutils.Vector((0, 80))
+                    viewer_emission.hide = True
+                    viewer_emission[luxcore_viewer_mark] = True
+                    viewer_emission.dls_type = "DISABLED"
+                    sockets_to_link.append((viewer_emission.outputs[0], viewer_matte.inputs["Emission"]))
+                    viewer_emission.gain = _calc_emission_viewer_gain(context)
+
+                    viewer_node = node_tree.nodes.new("NodeReroute")
+                    viewer_node.location = active_output.location + mathutils.Vector((-10, 55))
+                    viewer_node[luxcore_viewer_mark] = True
+                    viewer_node[luxcore_viewer_reroute_mark] = True
+                    # Diffuse color for albedo preview (material shading mode), emission for regular viewport render
+                    sockets_to_link.append((viewer_node.outputs[0], viewer_matte.inputs["Diffuse Color"]))
+                    sockets_to_link.append((viewer_node.outputs[0], viewer_emission.inputs["Color"]))
+
+                target_socket = viewer_node.inputs[0]
+                from ..nodes.sockets import LuxCoreSocketColor, LuxCoreSocketFloat, LuxCoreSocketVector
+                allowed_inputs = {LuxCoreSocketColor, LuxCoreSocketFloat, LuxCoreSocketVector}
+        elif node_tree.bl_idname == "luxcore_texture_nodes":
+            target_socket = active_output.inputs["Color"]
             allowed_inputs = target_socket.allowed_inputs
-            if viewer_node:
-                _remove_all_viewers(node_tree)
-        elif active_node.bl_idname.startswith("LuxCoreNodeShape"):
-            target_socket = active_output.inputs["Shape"]
+        elif node_tree.bl_idname == "luxcore_volume_nodes":
+            target_socket = active_output.inputs["Volume"]
             allowed_inputs = target_socket.allowed_inputs
-            if viewer_node:
-                _remove_all_viewers(node_tree)
-        elif active_node.bl_idname.startswith("LuxCoreNodeTex"):
-            if viewer_node:
-                # Existing viewer setup, find the emission node and update the gain
-                for node in node_tree.nodes:
-                    if luxcore_viewer_mark in node and node.bl_idname == "LuxCoreNodeMatEmission":
-                        node.gain = _get_emission_viewer_gain(context)
-                        break
-            else:
-                viewer_matte = node_tree.nodes.new("LuxCoreNodeMatMatte")
-                viewer_matte.location = active_output.location + mathutils.Vector((0, 50))
-                viewer_matte.hide = True
-                viewer_matte[luxcore_viewer_mark] = True
-                sockets_to_link.append((viewer_matte.outputs[0], active_output.inputs["Material"]))
-
-                viewer_emission = node_tree.nodes.new("LuxCoreNodeMatEmission")
-                viewer_emission.location = active_output.location + mathutils.Vector((0, 80))
-                viewer_emission.hide = True
-                viewer_emission[luxcore_viewer_mark] = True
-                viewer_emission.dls_type = "DISABLED"
-                sockets_to_link.append((viewer_emission.outputs[0], viewer_matte.inputs["Emission"]))
-                viewer_emission.gain = _get_emission_viewer_gain(context)
-
-                viewer_node = node_tree.nodes.new("NodeReroute")
-                viewer_node.location = active_output.location + mathutils.Vector((-10, 55))
-                viewer_node[luxcore_viewer_mark] = True
-                viewer_node[luxcore_viewer_reroute_mark] = True
-                # Diffuse color for albedo preview (material shading mode), emission for regular viewport render
-                sockets_to_link.append((viewer_node.outputs[0], viewer_matte.inputs["Diffuse Color"]))
-                sockets_to_link.append((viewer_node.outputs[0], viewer_emission.inputs["Color"]))
-
-            target_socket = viewer_node.inputs[0]
-            from ..nodes.sockets import LuxCoreSocketColor, LuxCoreSocketFloat, LuxCoreSocketVector
-            allowed_inputs = {LuxCoreSocketColor, LuxCoreSocketFloat, LuxCoreSocketVector}
 
         if not target_socket:
             return {"CANCELLED"}
