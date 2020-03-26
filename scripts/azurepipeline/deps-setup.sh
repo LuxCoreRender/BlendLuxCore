0260722335ad2ed99ca9e7b1ada35fcdd7d22e3e
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2020-03-26 10:38:23 +0100

    Forgot one update

--- scripts/azurepipeline/deps-setup.sh
+++ scripts/azurepipeline/deps-setup.sh
@@ -12,6 +12,6 @@ wget http://neurodebian.ovgu.de/debian/pool/main/n/neurodebian/neurodebian-popul
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/popularity-contest/popularity-contest_1.57ubuntu1_all.deb
 
 sudo dpkg -i popularity-contest_1.57ubuntu1_all.deb 
-sudo dpkg -i neurodebian-popularity-contest_0.39.0~nd14.04+1_all.deb 
+sudo dpkg -i neurodebian-popularity-contest_0.40.0~nd14.04+1_all.deb 
 sudo dpkg -i p7zip_16.02+dfsg-1~nd14.04+1_amd64.deb 
 sudo dpkg -i p7zip-full_16.02+dfsg-1~nd14.04+1_amd64.deb
