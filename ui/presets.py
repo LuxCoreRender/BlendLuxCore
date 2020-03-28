6ae9e42e3cd5e060da44dc23babefc3355ac3229
u3dreal <48415446+u3dreal@users.noreply.github.com>
2020-03-28 16:16:23 +0100

    Added Preset to LuxCore
    So far only the values in LightPaths are used but could be easiely expamded to all luxcore.config settings

---
+++ ui/presets.py
@@ -0,0 +1,52 @@
+from bl_operators.presets import AddPresetBase
+from bpy.types import Operator
+
+
+class AddPresetLuxcore(AddPresetBase, Operator):
+    '''Add an LuxCore Preset'''
+    bl_idname = "render.luxcore_preset_add"
+    bl_label = "Add LuxCore Preset"
+    preset_menu = "LUXCORE_RENDER_PT_luxcore_presets"
+
+    preset_defines = [
+        "luxcore = bpy.context.scene.luxcore.config"
+    ]
+
+    preset_values = [
+        "luxcore.path.depth_total",
+        "luxcore.path.depth_diffuse",
+        "luxcore.path.depth_glossy",
+        "luxcore.path.depth_specular",
+        "luxcore.path.use_clamping",
+        "luxcore.path.clamping",
+        "luxcore.light_strategy",
+        "luxcore.seed",
+        "luxcore.sampler",
+        "luxcore.sobol_adaptive_strength",
+        "luxcore.noise_estimation.warmup",
+        "luxcore.noise_estimation.step",
+        "luxcore.filter",
+        "luxcore.filter_width"
+    ]
+
+    preset_subdir = "BlendLuxCore"
+
+#classes = (
+#    AddPresetLuxcore,
+#)
+
+
+#def register():
+#    from bpy.utils import register_class
+#    for cls in classes:
+#        register_class(cls)
+
+
+#def unregister():
+#    from bpy.utils import unregister_class
+#    for cls in classes:
+#        unregister_class(cls)
+
+
+#if __name__ == "__main__":
+#    register()
--- ui/render_config.py
+++ ui/render_config.py
@@ -1,10 +1,11 @@
 import bpy
-from . import icons
+from . import icons, presets
 from .. import utils
 
 from bpy.types import Panel
 from bl_ui.properties_render import RENDER_PT_context
 from bl_ui.properties_render import RenderButtonsPanel
+from bl_ui.utils import PresetPanel
 
 
 def luxcore_render_draw(panel, context):
@@ -29,11 +30,21 @@ def luxcore_render_draw(panel, context):
     col.prop(config, "engine", expand=False)
 
     layout.operator("luxcore.use_cycles_settings")
+    
+class LUXCORE_RENDER_PT_luxcore_presets(PresetPanel, Panel):
+    bl_label = "LuxCore Presets"
+    preset_subdir = "BlendLuxCore"
+    preset_operator = "script.execute_preset"
+    preset_add_operator = "render.luxcore_preset_add"
+    COMPAT_ENGINES = {'LUXCORE'}
 
 class LUXCORE_RENDER_PT_lightpaths(RenderButtonsPanel, Panel):
     COMPAT_ENGINES = {"LUXCORE"}
     bl_label = "Light Paths"
     bl_order = 2
+    
+    def draw_header_preset(self, context):
+        LUXCORE_RENDER_PT_luxcore_presets.draw_panel_header(self.layout)
 
     def draw(self, context):
         pass
