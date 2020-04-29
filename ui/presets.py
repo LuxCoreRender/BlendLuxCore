daaa59b88d8621500cd86d81559a78c1942c1897
u3dreal <48415446+u3dreal@users.noreply.github.com>
2020-04-29 12:33:00 +0200

    removed add_preset class from presetpanel
    Now there is no way to create or delete luxcore panel

--- ui/presets.py
+++ ui/presets.py
@@ -1,29 +1,29 @@
 from bl_operators.presets import AddPresetBase
 from bpy.types import Operator
 
-class AddPresetLuxcore(AddPresetBase, Operator):
-    '''Add an LuxCore Preset'''
-    bl_idname = "render.luxcore_preset_add"
-    bl_label = "Add LuxCore Preset"
-    preset_menu = "LUXCORE_RENDER_PT_luxcore_presets"
-
-    preset_defines = [
-        "luxcore = bpy.context.scene.luxcore.config"
-    ]
-
-    preset_values = [
-        "luxcore.preset_version",
-        "luxcore.engine",
-        "luxcore.sampler",
-        "luxcore.path.hybridbackforward_enable",
-        "luxcore.photongi.enabled",
-        "luxcore.photongi.caustic_enabled",
-        "luxcore.photongi.indirect_enabled",
-        "luxcore.envlight_cache.enabled",
-        "luxcore.dls_cache.enabled"
-    ]
-
-    preset_subdir = "BlendLuxCore/Sampling"
+#class AddPresetLuxcore(AddPresetBase, Operator):
+#    '''Add an LuxCore Preset'''
+#    bl_idname = "render.luxcore_preset_add"
+#    bl_label = "Add LuxCore Preset"
+#    preset_menu = "LUXCORE_RENDER_PT_luxcore_presets"
+#
+#    preset_defines = [
+#        "luxcore = bpy.context.scene.luxcore.config"
+#    ]
+#
+#    preset_values = [
+#        "luxcore.preset_version",
+#        "luxcore.engine",
+#        "luxcore.sampler",
+#        "luxcore.path.hybridbackforward_enable",
+#        "luxcore.photongi.enabled",
+#        "luxcore.photongi.caustic_enabled",
+#        "luxcore.photongi.indirect_enabled",
+#        "luxcore.envlight_cache.enabled",
+#        "luxcore.dls_cache.enabled"
+#    ]
+#
+#    preset_subdir = "BlendLuxCore/Sampling"
     
 class Add_Image_Pipeline_PresetLuxcore(AddPresetBase, Operator):
     '''Add an LuxCore Image Pipeline Preset'''
