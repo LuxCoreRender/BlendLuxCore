ad945f5fa43803adf07c3e97fc9350c94ad67b17
u3dreal <48415446+u3dreal@users.noreply.github.com>
2020-05-10 18:26:57 +0200

    Update deps-setup.sh

--- scripts/azurepipeline/deps-setup.sh
+++ scripts/azurepipeline/deps-setup.sh
@@ -6,11 +6,13 @@ sudo apt-get install -y git zip wget bzip2
 
 #have to install g7zip 16.2 9.2 is too old install over alternative repo does not work.
 
+wget https://ftp.hetzner.de/ubuntu/packages/pool/main/d/dpkg/dpkg-dev_1.17.13ubuntu1_all.deb
 wget http://neurodebian.ovgu.de/debian/pool/main/p/p7zip/p7zip-full_16.02+dfsg-1~nd16.04+1_amd64.deb
 wget http://neurodebian.ovgu.de/debian/pool/main/p/p7zip/p7zip_16.02+dfsg-1~nd16.04+1_amd64.deb
 wget http://neurodebian.ovgu.de/debian/pool/main/n/neurodebian/neurodebian-popularity-contest_0.40.1~nd16.04+1_all.deb
 wget http://archive.ubuntu.com/ubuntu/pool/main/p/popularity-contest/popularity-contest_1.64ubuntu2_all.deb
 
+sudo dpkg -i dpkg-dev_1.17.13ubuntu1_all.deb
 sudo dpkg -i popularity-contest_1.64ubuntu2_all.deb 
 sudo dpkg -i neurodebian-popularity-contest_0.40.1~nd16.04+1_all.deb 
 sudo dpkg -i p7zip_16.02+dfsg-1~nd16.04+1_amd64.deb
