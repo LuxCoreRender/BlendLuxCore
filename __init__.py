8c43eb875e1db5a84b2ec358192cb8248bdc964b
Simon Wendsche <simon.wendsche@gmx.de>
2020-05-22 16:16:07 +0200

    formatting

--- __init__.py
+++ __init__.py
@@ -42,9 +42,10 @@ if platform.system() == "Windows":
     AddDllDirectory(bin_directory)
 
 if platform.system() in {"Linux", "Darwin"}:
+    # Required for downloads from the LuxCore Online Library
     import certifi
-    os.environ['SSL_CERT_FILE'] = certifi.where()
-    os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+    os.environ["SSL_CERT_FILE"] = certifi.where()
+    os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
 try:
     from .bin import pyluxcore
