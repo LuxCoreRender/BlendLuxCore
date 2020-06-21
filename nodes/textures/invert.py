4fcce04f9c51dce990e2480810265c1f1b13c8c5
Simon Wendsche <simon.wendsche@gmx.de>
2020-06-21 23:14:15 +0200

    add maximum input to invert node

--- nodes/textures/invert.py
+++ nodes/textures/invert.py
@@ -8,13 +8,14 @@ class LuxCoreNodeTexInvert(bpy.types.Node, LuxCoreNodeTexture):
 
     def init(self, context):
         self.add_input("LuxCoreSocketColor", "Color", (1, 1, 1))
+        self.add_input("LuxCoreSocketFloatPositive", "Maximum", 1)
 
         self.outputs.new("LuxCoreSocketColor", "Color")
 
     def sub_export(self, exporter, depsgraph, props, luxcore_name=None, output_socket=None):
         definitions = {
             "type": "subtract",
-            "texture1": 1,
+            "texture1": self.inputs["Maximum"].export(exporter, depsgraph, props),
             "texture2": self.inputs["Color"].export(exporter, depsgraph, props),
         }
 
