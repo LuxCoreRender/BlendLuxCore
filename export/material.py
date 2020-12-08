6b724d78e989d5f271d188e1ebfdcfa1102aac63
Simon Wendsche <simon.wendsche@gmx.de>
2020-12-08 22:12:54 +0100

    show warning if a material combines volumes with opacity < 1, as this can lead to artifacts

--- export/material.py
+++ export/material.py
@@ -1,5 +1,6 @@
 from ..bin import pyluxcore
 from .. import utils
+from ..utils import node as utils_node
 from ..nodes.output import get_active_output
 from ..utils.errorlog import LuxCoreErrorLog
 from . import cycles_node_reader
@@ -25,23 +26,25 @@ def convert(exporter, depsgraph, material, is_viewport_render, obj_name=""):
             return cycles_node_reader.convert(material, props, luxcore_name, obj_name)
 
         if node_tree is None:
-            msg = 'Material "%s": Missing node tree' % material.name
-            LuxCoreErrorLog.add_warning(msg, obj_name=obj_name)
+            LuxCoreErrorLog.add_warning(f'Material "{material.name}": Missing node tree', obj_name=obj_name)
             return fallback(luxcore_name)
 
         active_output = get_active_output(node_tree)
 
         if active_output is None:
-            msg = 'Node tree "%s": Missing active output node' % node_tree.name
-            LuxCoreErrorLog.add_warning(msg, obj_name=obj_name)
+            LuxCoreErrorLog.add_warning(f'Node tree "{node_tree.name}": Missing active output node', obj_name=obj_name)
             return fallback(luxcore_name)
+
+        if _has_volumes_and_transparency(node_tree, active_output):
+            msg = f'Material "{material.name}": Combining volumes and materials with opacity < 1 can lead to artifacts!'
+            LuxCoreErrorLog.add_warning(msg, obj_name=obj_name)
 
         # Now export the material node tree, starting at the output node
         active_output.export(exporter, depsgraph, props, luxcore_name)
 
         return luxcore_name, props
     except Exception as error:
-        msg = 'Material "%s": %s' % (material.name, error)
+        msg = f'Material "{material.name}": {error}'
         LuxCoreErrorLog.add_warning(msg, obj_name=obj_name)
         import traceback
         traceback.print_exc()
@@ -55,3 +58,14 @@ def fallback(luxcore_name=GLOBAL_FALLBACK_MAT):
     scene.materials.{mat_name}.kd = 0.5
     """.format(mat_name=luxcore_name))
     return luxcore_name, props
+
+
+def _has_volumes_and_transparency(node_tree, active_output):
+    if (utils_node.get_linked_node(active_output.inputs["Interior Volume"])
+            or utils_node.get_linked_node(active_output.inputs["Exterior Volume"])):
+        for node in node_tree.nodes:
+            if "Opacity" in node.inputs:
+                opacity_socket = node.inputs["Opacity"]
+                if opacity_socket.is_linked or opacity_socket.default_value < 1:
+                    return True
+    return False
