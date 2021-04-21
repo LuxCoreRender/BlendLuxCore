b68029295600e93769eda3ce65229ca5c44c421d
Alessandro Castagnini <35339386+acasta69@users.noreply.github.com>
2021-04-21 08:25:45 +0200

    Bump version to 2.6alpha0

--- __init__.py
+++ __init__.py
@@ -65,12 +65,12 @@ except ImportError as error:
 bl_info = {
     "name": "LuxCore",
     "author": "Simon Wendsche (B.Y.O.B.), Michael Klemm (neo2068), Philstix",
-    "version": (2, 5),
+    "version": (2, 6),
     "blender": (2, 80, 0),
     "category": "Render",
     "location": "Info header, render engine menu",
     "description": "LuxCore integration for Blender",
-    "warning": "",
+    "warning": "alpha0",
     "wiki_url": "https://wiki.luxcorerender.org/",
     "tracker_url": "https://github.com/LuxCoreRender/BlendLuxCore/issues/new",
 }
