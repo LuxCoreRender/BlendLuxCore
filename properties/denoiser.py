6eaecb7b633edcf435777a2133ce0f786a884d90
Odilkhan Yakubov <odilkhanyakubov@gmail.com>
2023-01-10 19:43:50 +0500

    - cosmetic changes

--- properties/denoiser.py
+++ properties/denoiser.py
@@ -31,8 +31,8 @@ class LuxCoreDenoiser(PropertyGroup):
 
     enabled: BoolProperty(name="", default=False, description="Enable/disable denoiser")
     type_items = [
-        ("BCD", "BCD", "Bayesian Collaborative Denoiser", 0),
-        ("OIDN", "OIDN", "Intel Open Image Denoiser", 1),
+        #("BCD", "BCD", "Bayesian Collaborative Denoiser", 0),
+        ("OIDN", "OpenImageDenoiser", "", 0),
     ]
     type: EnumProperty(name="Type", items=type_items, default="OIDN")
 
