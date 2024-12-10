5e5ee086a7ccc37ed6e2fa7851f726ad8c8989a6
Odilkhan Yakubov <odilkhanyakubov@gmail.com>
2024-12-11 01:40:08 +0500

    - Viewport rendering improved

--- draw/viewport.py
+++ draw/viewport.py
@@ -3,16 +3,15 @@ import gpu
 from gpu_extras.batch import batch_for_shader
 import math
 import os
-import numpy
+import numpy as np
 import subprocess
 import tempfile
 from shutil import which
-from os.path import dirname
+from os.path import dirname, join
 from ..bin import pyluxcore
 from .. import utils
 from ..utils import pfm
 
-NULL = 0
 
 class TempfileManager:
     _paths = {}
@@ -23,20 +22,17 @@ class TempfileManager:
 
     @classmethod
     def delete_files(cls, key):
-        if key not in cls._paths:
-            return
-        for path in cls._paths[key]:
-            if os.path.exists(path):
+        paths = cls._paths.pop(key, set())
+        for path in paths:
+            try:
                 os.remove(path)
-        cls._paths.pop(key, None)
+            except FileNotFoundError:
+                pass
 
     @classmethod
     def cleanup(cls):
-        for path_set in cls._paths.values():
-            for path in path_set:
-                if os.path.exists(path):
-                    os.remove(path)
-        cls._paths.clear()
+        for key in list(cls._paths):
+            cls.delete_files(key)
 
 
 class FrameBuffer:
@@ -47,53 +43,57 @@ class FrameBuffer:
         self._offset_x, self._offset_y = self._calc_offset(context, scene, self._border)
         self._pixel_size = int(scene.luxcore.viewport.pixel_size)
 
-        if utils.is_valid_camera(scene.camera) and not utils.in_material_shading_mode(context):
-            pipeline = scene.camera.data.luxcore.imagepipeline
-            self._transparent = pipeline.transparent_film
-        else:
-            self._transparent = False
-
-        if self._transparent:
-            bufferdepth = 4
-            self._buffertype = bgl.GL_RGBA
-            self._output_type = pyluxcore.FilmOutputType.RGBA_IMAGEPIPELINE
-        else:
-            bufferdepth = 3
-            self._buffertype = bgl.GL_RGB
-            self._output_type = pyluxcore.FilmOutputType.RGB_IMAGEPIPELINE
+        self._transparent = self._initialize_transparency(scene, context)
+        bufferdepth = 4 if self._transparent else 3
+        self._buffertype = bgl.GL_RGBA if self._transparent else bgl.GL_RGB
+        self._output_type = (
+            pyluxcore.FilmOutputType.RGBA_IMAGEPIPELINE
+            if self._transparent else
+            pyluxcore.FilmOutputType.RGB_IMAGEPIPELINE
+        )
 
         self.buffer = gpu.types.Buffer('FLOAT', [self._width * self._height * bufferdepth])
         self._init_opengl()
 
-        # Denoiser
-        self._noisy_file_path = self._make_denoiser_filepath("noisy")
-        self._albedo_file_path = self._make_denoiser_filepath("albedo")
-        self._normal_file_path = self._make_denoiser_filepath("normal")
-        self._denoised_file_path = self._make_denoiser_filepath("denoised")
-        current_dir = dirname(os.path.realpath(__file__))
-        addon_dir = dirname(current_dir)  # Go up one level
-        self._denoiser_path = which("oidnDenoise", path=os.path.join(addon_dir, "bin") + os.pathsep + os.environ["PATH"])
+        # Denoiser initialization
+        self._initialize_denoiser_paths()
         self._denoiser_process = None
         self.denoiser_result_cached = False
 
-    def _init_opengl(self):
-        width = self._width * self._pixel_size
-        height = self._height * self._pixel_size
+    def _initialize_transparency(self, scene, context):
+        if utils.is_valid_camera(scene.camera) and not utils.in_material_shading_mode(context):
+            return scene.camera.data.luxcore.imagepipeline.transparent_film
+        return False
 
-        position = (
-            (self._offset_x, self._offset_y),
-            (self._offset_x + width, self._offset_y),
-            (self._offset_x + width, self._offset_y + height),
-            (self._offset_x, self._offset_y + height),
-            (self._offset_x, self._offset_y),
-            (self._offset_x + width, self._offset_y + height)
+    def _initialize_denoiser_paths(self):
+        base_path = tempfile.gettempdir()
+        unique_id = id(self)
+        self._noisy_file_path = join(base_path, f"{unique_id}_noisy.pfm")
+        self._albedo_file_path = join(base_path, f"{unique_id}_albedo.pfm")
+        self._normal_file_path = join(base_path, f"{unique_id}_normal.pfm")
+        self._denoised_file_path = join(base_path, f"{unique_id}_denoised.pfm")
+
+        current_dir = dirname(os.path.realpath(__file__))
+        addon_dir = dirname(current_dir)
+        self._denoiser_path = which(
+            "oidnDenoise",
+            path=os.pathsep.join([join(addon_dir, "bin"), os.environ["PATH"]])
         )
 
+    def _init_opengl(self):
+        width, height = self._width * self._pixel_size, self._height * self._pixel_size
+        x, y = self._offset_x, self._offset_y
+
+        position = [
+            (x, y), (x + width, y), (x + width, y + height),
+            (x, y + height), (x, y), (x + width, y + height)
+        ]
+
         self.shader = gpu.shader.from_builtin('IMAGE')
-        self.batch = batch_for_shader(self.shader, 'TRIS', {
-            "pos": position,
-            "texCoord": ((0, 0), (1, 0), (1, 1), (0, 1), (0, 0), (1, 1)),
-        })
+        self.batch = batch_for_shader(
+            self.shader, 'TRIS',
+            {"pos": position, "texCoord": [(0, 0), (1, 0), (1, 1), (0, 1), (0, 0), (1, 1)]}
+        )
 
     def __del__(self):
         del self.buffer
@@ -115,82 +115,6 @@ class FrameBuffer:
         if self._pixel_size != int(scene.luxcore.viewport.pixel_size):
             return True
         return False
-
-    def _make_denoiser_filepath(self, name):
-        return os.path.join(tempfile.gettempdir(), f"{id(self)}_{name}.pfm")
-
-    def _save_denoiser_AOV(self, luxcore_session, film_output_type, path):
-        np_buffer = numpy.zeros((self._height, self._width, 3), dtype="float32")
-        luxcore_session.GetFilm().GetOutputFloat(film_output_type, np_buffer)
-        TempfileManager.track(id(self), path)
-        with open(path, "wb") as f:
-            pfm.save_pfm(f, np_buffer)
-
-    def start_denoiser(self, luxcore_session):
-        if not os.path.exists(self._denoiser_path):
-            raise Exception("Binary not found. Download it from https://github.com/OpenImageDenoise/oidn/releases")
-        if self._transparent:
-            self._alpha = numpy.zeros((self._height, self._width, 1), dtype="float32")
-            luxcore_session.GetFilm().GetOutputFloat(pyluxcore.FilmOutputType.ALPHA, self._alpha)
-
-        self._save_denoiser_AOV(luxcore_session, pyluxcore.FilmOutputType.RGB_IMAGEPIPELINE, self._noisy_file_path)
-        self._save_denoiser_AOV(luxcore_session, pyluxcore.FilmOutputType.ALBEDO, self._albedo_file_path)
-        self._save_denoiser_AOV(luxcore_session, pyluxcore.FilmOutputType.AVG_SHADING_NORMAL, self._normal_file_path)
-        TempfileManager.track(id(self), self._denoised_file_path)
-
-        args = [
-            self._denoiser_path,
-            "-hdr", self._noisy_file_path,
-            "-alb", self._albedo_file_path,
-            "-nrm", self._normal_file_path,
-            "-o", self._denoised_file_path,
-        ]
-        self._denoiser_process = subprocess.Popen(args)
-
-    def is_denoiser_active(self):
-        return self._denoiser_process is not None
-
-    def is_denoiser_done(self):
-        return self._denoiser_process.poll() is not None
-
-    def load_denoiser_result(self, scene):
-        self._denoiser_process = None
-        shape = (self._height * self._width * 3)
-        try:
-            with open(self._denoised_file_path, "rb") as f:
-                data, scale = pfm.load_pfm(f)
-            TempfileManager.delete_files(id(self))
-        except FileNotFoundError:
-            TempfileManager.delete_files(id(self))
-            raise Exception("Denoising failed, check console for details")
-
-        if self._transparent:
-            shape = (self._height * self._width * 4)
-            data = numpy.concatenate((data, self._alpha), axis=2)
-
-        data = numpy.resize(data, shape)
-        self.buffer[:] = data
-        self.denoiser_result_cached = True
-
-    def reset_denoiser(self):
-        """ Denoiser was not started yet or the user has triggered an update """
-        self.denoiser_result_cached = False
-
-        if self._denoiser_process:
-            print("Interrupting denoiser")
-            self._denoiser_process.terminate()
-            self._denoiser_process.communicate()
-            self._denoiser_process = None
-
-    def update(self, luxcore_session, scene):
-        luxcore_session.GetFilm().GetOutputFloat(self._output_type, self.buffer)
-
-    def draw(self, engine, context, scene):
-        format = 'RGBA16F' if self._transparent else 'RGB16F'
-        image = gpu.types.GPUTexture(size=(self._width, self._height), layers=0, is_cubemap=False, format=format,
-                                     data=self.buffer)
-        self.shader.uniform_sampler("image", image)
-        self.batch.draw(self.shader)
 
     def _calc_offset(self, context, scene, border):
         region_size = context.region.width, context.region.height
@@ -228,3 +152,80 @@ class FrameBuffer:
 
     def _cam_border_offset(self, aspect, base, border_min, region_width, view_camera_offset, zoom):
         return (0.5 - 2 * zoom * view_camera_offset) * region_width + aspect * base * (2 * border_min - 1)
+
+    def _make_denoiser_filepath(self, name):
+        return os.path.join(tempfile.gettempdir(), f"{id(self)}_{name}.pfm")
+
+    def _save_denoiser_AOV(self, luxcore_session, film_output_type, path):
+        np_buffer = np.zeros((self._height, self._width, 3), dtype="float32")
+        luxcore_session.GetFilm().GetOutputFloat(film_output_type, np_buffer)
+        TempfileManager.track(id(self), path)
+        with open(path, "wb") as f:
+            pfm.save_pfm(f, np_buffer)
+
+    def start_denoiser(self, luxcore_session):
+        if not os.path.exists(self._denoiser_path):
+            raise Exception("Binary not found. Download it from https://github.com/OpenImageDenoise/oidn/releases")
+
+        if self._transparent:
+            self._alpha = np.zeros((self._height, self._width, 1), dtype="float32")
+            luxcore_session.GetFilm().GetOutputFloat(pyluxcore.FilmOutputType.ALPHA, self._alpha)
+
+        self._save_denoiser_AOV(luxcore_session, pyluxcore.FilmOutputType.RGB_IMAGEPIPELINE, self._noisy_file_path)
+        self._save_denoiser_AOV(luxcore_session, pyluxcore.FilmOutputType.ALBEDO, self._albedo_file_path)
+        self._save_denoiser_AOV(luxcore_session, pyluxcore.FilmOutputType.AVG_SHADING_NORMAL, self._normal_file_path)
+        TempfileManager.track(id(self), self._denoised_file_path)
+
+        args = [
+            self._denoiser_path,
+            "-hdr", self._noisy_file_path,
+            "-alb", self._albedo_file_path,
+            "-nrm", self._normal_file_path,
+            "-o", self._denoised_file_path,
+        ]
+        self._denoiser_process = subprocess.Popen(args)
+
+    def is_denoiser_active(self):
+        return self._denoiser_process is not None
+
+    def is_denoiser_done(self):
+        return self._denoiser_process.poll() is not None
+
+    def load_denoiser_result(self, scene):
+        self._denoiser_process = None
+        shape = (self._height * self._width * 3)
+        try:
+            with open(self._denoised_file_path, "rb") as f:
+                data, scale = pfm.load_pfm(f)
+            TempfileManager.delete_files(id(self))
+        except FileNotFoundError:
+            TempfileManager.delete_files(id(self))
+            raise Exception("Denoising failed, check console for details")
+
+        if self._transparent:
+            shape = (self._height * self._width * 4)
+            data = np.concatenate((data, self._alpha), axis=2)
+
+        data = np.resize(data, shape)
+        self.buffer[:] = data
+        self.denoiser_result_cached = True
+
+    def reset_denoiser(self):
+        """ Denoiser was not started yet or the user has triggered an update """
+        self.denoiser_result_cached = False
+
+        if self._denoiser_process:
+            print("Interrupting denoiser")
+            self._denoiser_process.terminate()
+            self._denoiser_process.communicate()
+            self._denoiser_process = None
+
+    def update(self, luxcore_session, scene):
+        luxcore_session.GetFilm().GetOutputFloat(self._output_type, self.buffer)
+
+    def draw(self, engine, context, scene):
+        format = 'RGBA16F' if self._transparent else 'RGB16F'
+        image = gpu.types.GPUTexture(size=(self._width, self._height), layers=0, is_cubemap=False, format=format,
+                                     data=self.buffer)
+        self.shader.uniform_sampler("image", image)
+        self.batch.draw(self.shader)
