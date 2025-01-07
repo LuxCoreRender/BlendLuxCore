9646862bcd3943a64e9c8ca6b1facbbb70d6d0f4
CodeFHD <joho.hini@gmail.com>
2025-01-07 23:32:35 +0100

    Update LoL to Blender API changes

--- draw/lol/viewport.py
+++ draw/lol/viewport.py
@@ -22,7 +22,7 @@
 #
 # #####
 
-import bgl, blf
+import blf
 import bpy
 import gpu
 import math
@@ -49,6 +49,7 @@ def draw_progress(x, y, text='', percent=None, color=(0, 1, 0, 1)):
 
 
 def draw_rect(x, y, width, height, color):
+    gpu.state.blend_set('ALPHA')
     xmax = x + width
     ymax = y + height
     points = [[x, y],  # [x, y]
@@ -58,13 +59,13 @@ def draw_rect(x, y, width, height, color):
               ]
     indices = ((0, 1, 2), (2, 3, 0))
 
-    shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
+    shader = gpu.shader.from_builtin('UNIFORM_COLOR')
     batch = batch_for_shader(shader, 'TRIS', {"pos": points}, indices=indices)
 
     shader.bind()
     shader.uniform_float("color", color)
-    bgl.glEnable(bgl.GL_BLEND)
     batch.draw(shader)
+    gpu.state.blend_set('NONE')
 
 
 def draw_line2d(x1, y1, x2, y2, width, color):
@@ -73,31 +74,28 @@ def draw_line2d(x1, y1, x2, y2, width, color):
 
     indices = (
         (0, 1),)
-    bgl.glEnable(bgl.GL_BLEND)
-    bgl.glEnable(bgl.GL_LINE_SMOOTH)
 
-    shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
+    gpu.state.blend_set('ALPHA')
+    shader = gpu.shader.from_builtin('UNIFORM_COLOR')
     batch = batch_for_shader(shader, 'LINES', {"pos": coords}, indices=indices)
     shader.bind()
     shader.uniform_float("color", color)
     batch.draw(shader)
+    gpu.state.blend_set('NONE')
 
 
 def draw_lines(vertices, indices, color):
-    bgl.glEnable(bgl.GL_BLEND)
-    bgl.glEnable(bgl.GL_LINE_SMOOTH)
-    bgl.glLineWidth(2)
-
-    shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+    gpu.state.blend_set('ALPHA')
+    shader = gpu.shader.from_builtin('UNIFORM_COLOR')
     batch = batch_for_shader(shader, 'LINES', {"pos": vertices}, indices=indices)
     shader.bind()
     shader.uniform_float("color", color)
     batch.draw(shader)
-
+    gpu.state.blend_set('NONE')
 
 def draw_rect_3d(coords, color):
     indices = [(0, 1, 2), (2, 3, 0)]
-    shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+    shader = gpu.shader.from_builtin('UNIFORM_COLOR')
     batch = batch_for_shader(shader, 'TRIS', {"pos": coords}, indices=indices)
     shader.uniform_float("color", color)
     batch.draw(shader)
@@ -146,8 +144,6 @@ def draw_bbox(location, rotation, bbox_min, bbox_max, progress=None, color=(0, 1
 
 
 def draw_image(x, y, width, height, image, transparency, crop=(0, 0, 1, 1)):
-    # draw_rect(x,y, width, height, (.5,0,0,.5))
-
     coords = [
         (x, y), (x + width, y),
         (x, y + height), (x + width, y + height)]
@@ -160,32 +156,20 @@ def draw_image(x, y, width, height, image, transparency, crop=(0, 0, 1, 1)):
 
     indices = [(0, 1, 2), (2, 1, 3)]
 
-    shader = gpu.shader.from_builtin('2D_IMAGE')
+    gpu.state.blend_set('ALPHA')
+    shader = gpu.shader.from_builtin('IMAGE')
     batch = batch_for_shader(shader, 'TRIS',
                              {"pos": coords,
                               "texCoord": uvs},
                              indices=indices)
 
-    # send image to gpu if it isn't there already
-    if image.gl_load():
-        raise Exception()
-
-    # texture identifier on gpu
-    texture_id = image.bindcode
-
-    # in case someone disabled it before
-    bgl.glEnable(bgl.GL_BLEND)
-
-    # bind texture to image unit 0
-    bgl.glActiveTexture(bgl.GL_TEXTURE0)
-    bgl.glBindTexture(bgl.GL_TEXTURE_2D, texture_id)
+    texture = gpu.texture.from_image(image)
 
     shader.bind()
-    # tell shader to use the image that is bound to image unit 0
-    shader.uniform_int("image", 0)
+    shader.uniform_sampler("image", texture)
     batch.draw(shader)
 
-    bgl.glDisable(bgl.GL_TEXTURE_2D)
+    gpu.state.blend_set('NONE')
 
 
 def draw_downloader(x, y, percent=0, img=None):
@@ -205,7 +189,7 @@ def draw_text(text, x, y, size, color=(1, 1, 1, 0.5)):
     # bgl.glColor4f(*color)
     blf.color(font_id, color[0], color[1], color[2], color[3])
     blf.position(font_id, x, y, 0)
-    blf.size(font_id, size, 72)
+    blf.size(font_id, size)
     blf.draw(font_id, text)
 
 
