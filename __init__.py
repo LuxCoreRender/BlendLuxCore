3d0cea599cca11234141b2423c0908a77a5aac03
CodeFHD <joho.hini@gmail.com>
2025-03-24 17:33:54 +0100

    remove obsolte check for macOS version

--- __init__.py
+++ __init__.py
@@ -15,11 +15,6 @@ import addon_utils
 if bpy.app.version < (4, 2, 0):
     raise Exception("\n\nUnsupported Blender version. 4.2 or higher is required by BlendLuxCore.")
 
-if platform.system() == "Darwin":
-    mac_version = tuple(map(int, platform.mac_ver()[0].split(".")))
-    if mac_version < (10, 9, 0):
-        raise Exception("\n\nUnsupported Mac OS version. 10.9 or higher is required.")
-
 if platform.system() in {"Linux", "Darwin"}:
     # Required for downloads from the LuxCore Online Library
     import certifi
