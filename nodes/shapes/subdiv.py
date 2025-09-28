1d11b9a4acbd4ce4042024771899a64dc9408616
howetuft <howetuft@gmail.com>
2025-09-28 09:59:58 +0200

    Reformat nodes/shapes/subdiv.py

--- nodes/shapes/subdiv.py
+++ nodes/shapes/subdiv.py
@@ -8,10 +8,18 @@ class LuxCoreNodeShapeSubdiv(LuxCoreNodeShape, bpy.types.Node):
     bl_label = "Subdivision"
     bl_width_default = 150
 
-    max_level: IntProperty(name="Max. Level", default=2, min=1,
-                           update=utils_node.force_viewport_mesh_update)
-    max_edge_screen_size: FloatProperty(name="Max. Edge Screen Size", default=0, min=0,
-                                        update=utils_node.force_viewport_mesh_update)
+    max_level: IntProperty(
+        name="Max. Level",
+        default=2,
+        min=1,
+        update=utils_node.force_viewport_mesh_update,
+    )
+    max_edge_screen_size: FloatProperty(
+        name="Max. Edge Screen Size",
+        default=0,
+        min=0,
+        update=utils_node.force_viewport_mesh_update,
+    )
 
     def init(self, context):
         self.add_input("LuxCoreSocketShape", "Shape")
@@ -24,8 +32,12 @@ class LuxCoreNodeShapeSubdiv(LuxCoreNodeShape, bpy.types.Node):
     def export_shape(self, exporter, depsgraph, props, base_shape_name):
         definitions = {
             "type": "subdiv",
-            "source": self.inputs["Shape"].export_shape(exporter, depsgraph, props, base_shape_name),
+            "source": self.inputs["Shape"].export_shape(
+                exporter, depsgraph, props, base_shape_name
+            ),
             "maxlevel": self.max_level,
             "maxedgescreensize": self.max_edge_screen_size,
         }
-        return self.create_props(props, definitions, self.make_shape_name(base_shape_name))
+        return self.create_props(
+            props, definitions, self.make_shape_name(base_shape_name)
+        )
