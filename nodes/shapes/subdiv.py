bcd50a7e95909f15d3bd944a0f564e8ee81f3676
howetuft <howetuft@gmail.com>
2026-03-25 20:23:14 +0100

    Subdiv: Expose sharpness threshold and crease weight parameters

--- nodes/shapes/subdiv.py
+++ nodes/shapes/subdiv.py
@@ -25,6 +25,29 @@ class LuxCoreNodeShapeSubdiv(LuxCoreNodeShape, bpy.types.Node):
         default=False,
         update=utils_node.force_viewport_mesh_update,
     )
+    sharpness_threshold: FloatProperty(
+        name="Sharpness Threshold (radians)",
+        description=(
+            "If the dihedral angle between two adjacent faces is less than this "
+            "threshold, the shared edge is considered sharp and will be treated "
+            "as such during subdivision."
+            "\nENHANCED ONLY"
+        ),
+        default=0.521,
+        min=0,
+        update=utils_node.force_viewport_mesh_update,
+    )
+    crease_weight: FloatProperty(
+        name="Crease weight (from 0.0 to 10.0)",
+        description=(
+            "Crease weight for sharp edges. High values make edges sharper. "
+            "Should stay in [0.0, 10.0]."
+            "\nENHANCED ONLY"
+        ),
+        default=10.0,
+        min=0,
+        update=utils_node.force_viewport_mesh_update,
+    )
 
     def init(self, context):
         self.add_input("LuxCoreSocketShape", "Shape")
@@ -34,6 +57,8 @@ class LuxCoreNodeShapeSubdiv(LuxCoreNodeShape, bpy.types.Node):
         layout.prop(self, "max_level")
         layout.prop(self, "max_edge_screen_size")
         layout.prop(self, "enhanced")
+        layout.prop(self, "sharpness_threshold")
+        layout.prop(self, "crease_weight")
 
     def export_shape(self, exporter, depsgraph, props, base_shape_name):
         definitions = {
@@ -44,6 +69,8 @@ class LuxCoreNodeShapeSubdiv(LuxCoreNodeShape, bpy.types.Node):
             "maxlevel": self.max_level,
             "maxedgescreensize": self.max_edge_screen_size,
             "enhanced": self.enhanced,
+            "sharpnessthreshold": self.sharpness_threshold,
+            "creaseweight": self.crease_weight
         }
         return self.create_props(
             props, definitions, self.make_shape_name(base_shape_name)
